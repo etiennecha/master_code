0ea5fe77e48ae923dd618b191b5f33f95c0da8d1
etna <echamayou@gmail.com>
2014-03-03 08:33:27 +0100

    Start competition analysis

---
+++ code_gasoline/execution_paper/competition/analysis_competition.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import add_to_path_sub
+from add_to_path_sub import path_data
+from generic_master_price import *
+from generic_master_info import *
+from functions_string import *
+
+path_dir_built_paper = os.path.join(path_data, 'data_gasoline', 'data_built', 'data_paper')
+
+path_dir_built_json = os.path.join(path_dir_built_paper, 'data_json')
+path_diesel_price = os.path.join(path_dir_built_json, 'master_price_diesel.json')
+path_info = os.path.join(path_dir_built_json, 'master_info_diesel.json')
+path_ls_ls_competitors = os.path.join(path_dir_built_json, 'ls_ls_competitors.json')
+path_ls_tuple_competitors = os.path.join(path_dir_built_json, 'ls_tuple_competitors.json')
+
+path_dir_source = os.path.join(path_data, 'data_gasoline', 'data_source')
+path_dict_brands = os.path.join(path_dir_source, 'data_other', 'dict_brands.json')
+path_csv_insee_data = os.path.join(path_dir_source, 'data_other', 'data_insee_extract.csv')
+
+path_dir_insee = os.path.join(path_data, 'data_insee')
+path_dict_dpts_regions = os.path.join(path_dir_insee, 'dpts_regions', 'dict_dpts_regions.json')
+
+master_price = dec_json(path_diesel_price)
+master_info = dec_json(path_info)
+ls_ls_competitors = dec_json(path_ls_ls_competitors)
+ls_tuple_competitors = dec_json(path_ls_tuple_competitors)
+dict_brands = dec_json(path_dict_brands)
+dict_dpts_regions = dec_json(path_dict_dpts_regions)
+
+# TODO: build df w/ nb of competitors within 1... 10 km, closest comp, closest supermarket
+# TODO: merge DF with info stations (should finally serve in price dispersion?)
+
