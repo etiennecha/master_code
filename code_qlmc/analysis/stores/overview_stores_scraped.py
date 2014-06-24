ce1ebddc6b57c49d3edad8adc5131d1b81187526
etiennecha <echamayou@gmail.com>
2014-06-24 17:27:59 +0200

    update qlmc

--- code_qlmc/analysis/stores/overview_stores_scraped.py
+++ code_qlmc/analysis/stores/overview_stores_scraped.py
@@ -85,7 +85,9 @@ for x in ls_chain_full_info[0]:
                          ' '.join(gps),
                          address[:re_zip.start()].strip(),
                          address[re_zip.start()+1:re_zip.end()].strip(),
-                         re.split(u'cedex', address[re_zip.end():], flags=re.IGNORECASE)[0].strip()])
+                         re.split(u'cedex', 
+                                  address[re_zip.end():],
+                                  flags=re.IGNORECASE)[0].strip()])
 df_auchan = pd.DataFrame(ls_rows_auchan, columns = ls_columns)
 
 # CARREFOUR (2354, of which 1079 Carrefour and Carrefour markets: keep only hyper)
@@ -108,7 +110,7 @@ for x in ls_chain_full_info[2]:
                           x['address'], # fix and regeocode
                           x['zip'],
                           x['city']])
-df_leclerc = pd.DataFrame(ls_rows_leclerc)
+df_leclerc = pd.DataFrame(ls_rows_leclerc, columns = ls_columns)
 
 
 # SYSTEME U (1147 of which 47 hypers)
@@ -120,7 +122,7 @@ for dict_store in ls_chain_general_info[4]:
                     dict_store['street'],
                     dict_store['zip'],
                     dict_store['city']])
-df_u = pd.DataFrame(ls_rows_u)
+df_u = pd.DataFrame(ls_rows_u, columns = ls_columns)
 
 # SUPER CASINO (update... may not have changed and quite a few gps missing)
 def get_casino_type_name(word):
@@ -150,6 +152,7 @@ for x in ls_chain_general_info[7]:
                          x['address'][1][6:].strip().replace(u'&#039;', u"'")])
 df_casino = pd.DataFrame(ls_rows_casino,  columns = ls_columns)
 df_casino = df_casino[df_casino['type'] != u'Casino Cafétéria']
+df_casino.reset_index(drop=True, inplace = True)
 # todo : duplicate detection + a lot of gps missing?
 
 ## GEANT CASINO (110: included in CASINO)
@@ -163,11 +166,11 @@ for dict_store in ls_chain_general_info[5]:
   ls_rows_cora.append([u'Cora',
                        dict_store['name'],
                        ' '.join(map(lambda x: u'{:2.4f}'.format(float(x)), dict_store['gps'])),
+                       dict_store['street'],
                        dict_store['zip_city'][0:6].strip(),
                        re.split(u'cedex',
                                 dict_store['zip_city'][6:].replace(u'&lt;br&gt;', u''),
-                                flags=re.IGNORECASE)[0].strip(),
-                       dict_store['street']])
+                                flags=re.IGNORECASE)[0].strip()])
 df_cora = pd.DataFrame(ls_rows_cora, columns = ls_columns)
 
 # MONOPRIX
@@ -176,22 +179,23 @@ for ls_store in ls_chain_general_info[8]:
   ls_rows_monoprix.append([ls_store[0],
                            ls_store[1],
                            '',
+                           ls_store[3][2],
                            ls_store[3][1],
-                           ls_store[3][0],
-                           ls_store[3][2]])
+                           ls_store[3][0]])
 df_monoprix = pd.DataFrame(ls_rows_monoprix, columns = ls_columns)
 
 # FRANPRIX (supermarkets or smaller?... scrap zip code too?)
 ls_rows_franprix = []
 for dict_store in ls_chain_general_info[6]:
   re_street = re.search(u'\s-\s', dict_store['address'])
+  city = dict_store['address'][:re_street.start()].replace(u'&#039;', u'').strip()
+  street = dict_store['address'][re_street.end():].strip().replace(u'&#039;', u'').strip()
   ls_rows_franprix.append([u'Franprix',
-                           u'Franprix ' +\
-                             dict_store['address'][:re_street.start()].replace(u'&#039;', u'').strip(),
+                           u'Franprix ' + city,
                            ' '.join(map(lambda x: u'{:2.4f}'.format(float(x)), dict_store['gps'])),
+                           street,
                            u'',
-                           dict_store['address'][:re_street.start()].replace(u'&#039;', u'').strip(),
-                           dict_store['address'][re_street.end():].strip()])
+                           city])
 df_franprix = pd.DataFrame(ls_rows_franprix, columns = ls_columns)
 
 # OTHER DATA
@@ -254,12 +258,15 @@ df_itm_kml = pd.DataFrame(ls_chain_kml[1],
                           columns = ['name', 'gps'])
 df_itm_kml['name_2'] = df_itm_kml['name'].apply(lambda x: standardize_intermarche(\
                                                             str_low_noacc(x.decode('latin-1'))))
+df_itm_kml['gps'] = df_itm_kml['gps'].apply(lambda y: ' '.join(\
+                                              map(lambda x: u'{:2.4f}'.format(float(x)), y)))
+
 se_itm_kml_vc = df_itm_kml['name_2'].value_counts()
 se_itm_kml_unique = se_itm_kml_vc[se_itm_kml_vc == 1]
 str_kml_unique = u'|'.join(se_itm_kml_unique.index)
 df_itm_kml = df_itm_kml[df_itm_kml['name_2'].str.contains(str_kml_unique)]
 
-# match
+# match: old intermarche website (including entity names) vs. kml
 ls_itm_matched = [x for x in df_itm['name_2'].values if x in  df_itm_kml['name_2'].values]
 ls_itm_no = [x for x in df_itm['name_2'].values if x not in df_itm_kml['name_2'].values]
 
@@ -269,7 +276,74 @@ df_itm_all = df_itm.join(df_itm_kml, rsuffix='_kml')
 #df_itm_all_o = df_itm.join(df_itm_kml, rsuffix='_kml', how='outer')
 df_itm_all['gps'] = df_itm_all['gps_kml']
 del(df_itm_all['gps_kml'])
-print df_itm_all[ls_columns].to_string()
+# print df_itm_all[['name', 'gps', 'street', 'zip', 'city']].to_string()
+
+# match 2: old website with kml vs. new website (including type of supermarket)
+
+# clean street in itm
+def clean_itm_street(word):
+  ls_replace = [[u'\n', u''],
+                [u'\t', u''],
+                [u"'",  u' '],
+                [u'"',  u' ']]
+  for old, new in ls_replace:
+    word = word.replace(old, new)
+  word = u' '.join([x for x in word.split(u' ') if x])
+  return word
+
+df_itm_all['street'] = df_itm_all['street'].apply(lambda x: clean_itm_street(x).lower())
+df_intermarche['street'] = df_intermarche['street'].apply(lambda x: clean_itm_street(x).lower())
+
+df_intermarche['street_zip'] = df_intermarche['street'] + u' ' + df_intermarche['zip']
+df_itm_all['street_zip'] = df_itm_all['street'] + u' ' + df_itm_all['zip']
+ls_itm_matched_2 = [x for x in df_intermarche['street_zip'].values if x in df_itm_all['street_zip'].values]
+ls_itm_no_2 = [x for x in df_intermarche['street_zip'].values if x not in df_itm_all['street_zip'].values]
+
+df_itm_all.set_index('street_zip', inplace = True)
+df_intermarche.set_index('street_zip', inplace = True)
+df_intermarche_all = df_intermarche.join(df_itm_all[['name', 'gps', 'city']],
+                                         rsuffix='_o',
+                                         how = 'outer')
+df_intermarche_all.reset_index(drop = True, inplace = True)
+
+# first matching
+len(df_intermarche_all[(~pd.isnull(df_intermarche_all['name_o'])) &\
+                       (~pd.isnull(df_intermarche_all['gps_o']))])
+
+# full matching
+len(df_intermarche_all[(~pd.isnull(df_intermarche_all['type'])) &\
+                        (~pd.isnull(df_intermarche_all['gps_o']))])
+
+pd.set_option('display.max_colwidth', 30)
+ls_disp_itm = ['name', 'street', 'zip', 'city', 'name_o', 'gps_o', 'city_o']
+# print df_intermarche_all[ls_disp_itm][0:100].to_string()
 
 # todo: correct in kml
 # "a. midel" => "midel"
+
+df_intermarche_all.drop(['name_o', 'city_o', 'gps'], 1, inplace = True)
+df_intermarche_all.rename(columns = {'gps_o' : 'gps'}, inplace = True)
+
+# DF ALL STORES IN FRANCE
+
+ls_df_stores_france = [df_auchan,
+                       df_carrefour, # exclude small formats
+                       df_leclerc,
+                       df_u,
+                       df_casino, # exclude duplicates?
+                       df_cora,
+                       df_monoprix, # no gps for now + duplicates to exclude
+                       df_franprix,
+                       df_intermarche_all] # partial gps info
+
+df_stores_france = pd.concat(ls_df_stores_france, ignore_index = True)
+
+# No gps
+len(df_stores_france[(df_stores_france['gps'] == '') | (pd.isnull(df_stores_france['gps']))])
+
+print df_stores_france[ls_columns].to_string()
+
+# df_itm_all[df_itm_all['city'] == 'volgelsheim']
+# duplicate ???
+
+# todo: DF HD: dia, leaderprice, lidl, aldi, netto
