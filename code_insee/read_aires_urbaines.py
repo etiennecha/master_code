e030287c9e41584609cd6ab676475ef049983a8e
etna <echamayou@gmail.com>
2014-07-16 10:18:22 +0200

    update aire urbaine data extraction

--- code_insee/read_aires_urbaines.py
+++ code_insee/read_aires_urbaines.py
@@ -15,7 +15,9 @@ path_dir_communes = os.path.join(path_dir_insee, 'communes')
 # AIRES URBAINES 2010
 # #######################
 
-path_xls_au = os.path.join(path_dir_communes, 'AiresUrbaines', 'AU2010.xls')
+path_xls_au = os.path.join(path_dir_communes,
+                           'AiresUrbaines',
+                           'AU2010.xls')
 wb_au = xlrd.open_workbook(path_xls_au)
 print 'Sheets in file', wb_au.sheet_names()
 
@@ -69,7 +71,9 @@ df_au_com.index = df_au_com['CODGEO'] # set index back to CODGEO
 # #######################
 # POPULATION 2010
 
-path_xls_population = os.path.join(path_dir_communes, 'Pop', 'base-cc-evol-struct-pop-2010.xls')
+path_xls_population = os.path.join(path_dir_communes,
+                                   'Pop',
+                                   'base-cc-evol-struct-pop-2010.xls')
 
 wb_population = xlrd.open_workbook(path_xls_population)
 print 'Sheets in file', wb_population.sheet_names()
@@ -93,7 +97,9 @@ df_population.index = df_population['CODGEO']
 # #######################
 # LOGEMENT 2010
 
-path_xls_logement = os.path.join(path_dir_communes, 'Logement', 'base-cc-logement-2010.xls')
+path_xls_logement = os.path.join(path_dir_communes,
+                                 'Logement',
+                                 'base-cc-logement-2010.xls')
 
 wb_logement = xlrd.open_workbook(path_xls_logement)
 print 'Sheets in file', wb_logement.sheet_names()
@@ -114,51 +120,96 @@ ls_logement_select_columns = [u'CODGEO', u'LIBGEO',
                               u'P10_LOG', u'P10_MEN', u'P10_PMEN',
                               u'P10_RP_VOIT1P', 'P10_RP_VOIT1', 'P10_RP_VOIT2P']
 df_logement = df_logement[ls_logement_select_columns] 
+df_logement.index = df_logement['CODGEO']
 
-# #################
-# BUILD/ENRICH DF AU
-# #################
+# #######################
+# REVENUS FISCAUX 2010
 
-df_au_com['POP10'] = df_population['P10_POP'].convert_objects(convert_numeric = True)
-df_au_com['SUPERF'] = df_population['SUPERF'].convert_objects(convert_numeric = True)
-# todo: groupby...
+path_xls_revenus = os.path.join(path_dir_communes,
+                                'Revenus_fiscaux',
+                                'base-cc-rev-fisc-loc-menage-10.xls')
 
-## #################################### 
-## 1E/ Merge INSEE communes dataframes
-#
-#print '\n', df_uu.info()
-#print '\n', df_au.info()
-#print '\n', df_population.info()
-#print '\n', df_logement.info()
-#del(df_au['LIBGEO'], df_population['LIBGEO'], df_logement['LIBGEO'])
-#df_insee = pd.merge(df_uu, df_au, on = 'CODGEO')
-#df_insee = pd.merge(df_insee, df_population, on = 'CODGEO')
-#df_insee = pd.merge(df_insee, df_logement, on = 'CODGEO') 
-#
-#print '\nINSEE EXTRACT\n', df_insee.info()
-#
-#path_dir_insee_built = os.path.join(path_dir_insee, 'data_extracts')
-#df_insee.to_csv(os.path.join(path_dir_insee_built, 'data_insee_extract.csv'),
-#                  float_format='%.3f', encoding='utf-8', index=False)
+wb_revenus = xlrd.open_workbook(path_xls_revenus)
+print 'Sheets in file', wb_revenus.sheet_names()
+
+sh_revenus_com = wb_revenus.sheet_by_name(u'REVME_COM')
+ls_columns = sh_revenus_com.row_values(5)
+ls_rows = [sh_revenus_com.row_values(i) for i in range(6, sh_revenus_com.nrows)]
+df_revenus_com = pd.DataFrame(ls_rows, columns = ls_columns, dtype = str)
+
+sh_revenus_arm = wb_revenus.sheet_by_name(u'REVME_ARM')
+ls_columns = sh_revenus_arm.row_values(5)
+ls_rows = [sh_revenus_arm.row_values(i) for i in range(6, sh_revenus_arm.nrows)]
+df_revenus_arm = pd.DataFrame(ls_rows, columns = ls_columns, dtype = str)
+
+df_revenus = pd.concat([df_revenus_com, df_revenus_arm], ignore_index = True)
+ls_revenus_select_columns = [u'CODGEO', u'MENFIS10', u'PMENFIS10', u'MENIMP10',
+                             u'QUAR1UC10', u'QUAR2UC10', u'QUAR3UC10', u'RDUC10',
+                             u'PTSA10', u'PPEN10', u'PBEN10', u'PAUT10']
+df_revenus = df_revenus[ls_revenus_select_columns] 
+df_revenus.index = df_revenus['CODGEO']
+
+# Stats provided at AU (& UU) level!
+sh_revenus_au = wb_revenus.sheet_by_name(u'REVME_AU2010')
+ls_columns = sh_revenus_au.row_values(5)
+ls_rows = [sh_revenus_au.row_values(i) for i in range(6, sh_revenus_au.nrows)]
+df_revenus_au = pd.DataFrame(ls_rows, columns = ls_columns, dtype = str)
+df_revenus_au = df_revenus_au[ls_revenus_select_columns] 
+df_revenus_au.set_index('CODGEO', inplace=True)
+
+# ######################
+# BUILD DF AU AGGREGATED
+# ######################
+
+# population variables
+ls_population_var_names = ['P10_POP', 'SUPERF']
+for var_name in ls_population_var_names:
+  df_au_com[var_name] = df_population[var_name].convert_objects(convert_numeric = True)
+df_au_com['POPDENSITY10'] = df_au_com['P10_POP'] / df_au_com['SUPERF']
+# logement variables
+ls_logement_var_names = [u'P10_LOG', u'P10_MEN', u'P10_PMEN',
+                         u'P10_RP_VOIT1P', 'P10_RP_VOIT1', 'P10_RP_VOIT2P']
+for var_name in ls_logement_var_names:
+  df_au_com[var_name] = df_logement[var_name].convert_objects(convert_numeric = True)
+
+# could do more elaborate groupby (with dedicated function)
+# http://pandas.pydata.org/pandas-docs/dev/groupby.html
+# http://stackoverflow.com/questions/15262134/ (ctd on next line)
+# apply-different-functions-to-different-items-in-group-object-python-pandas
+df_au_agg = df_au_com[['AU2010'] +\
+                      ls_population_var_names +\
+                      ls_logement_var_names].groupby('AU2010').sum()
+df_au_agg['POPDENSITY10'] = df_au_agg['P10_POP'] / df_au_agg['SUPERF']
+## next two lines for temp display / can be dropped if better groupby
+#df_au_info.index = df_au_info['CODGEO']
+#df_au_agg['LIBAU2010'] = df_au_info['LIBGEO']
+df_au_agg = pd.merge(df_au_agg, df_revenus_au, left_index = True, right_index = True)
+
+pd.set_option('float_format', '{:10,.0f}'.format)
+#print df_au_agg.to_string()
+
+# ###################
+# BUILD FINAL DF AU
+# ###################
+
+# need df with au info at commune level (i.e. index = insee code)
+# keep TAU2010, CATAEU2010 and if commune = center or not
+df_au_com_final = df_au_com[['LIBGEO', 'AU2010', 'LIBAU2010',
+                         'TAU2010', 'CATAEU2010',
+                         'AU_Center', 'CODGEO_Center']]
+df_au_com_final = pd.merge(df_au_com_final, df_au_agg, how='left', # check how...
+                           left_on = 'AU2010', right_index = True)
+df_au_com_final.sort(inplace=True) # sort by index (was sorted by AU2010)
+#print df_au_final[0:100].to_string()
+
+path_dir_insee_built = os.path.join(path_dir_insee, 'data_extracts')
+df_au_com_final.to_csv(os.path.join(path_dir_insee_built, 'df_au_com_final.csv'),
+                       float_format='%.2f', encoding='utf-8', 
+                       index=True, index_label=u'CODGEO')
+
+
+## BACKUP
+
 ##path_dir_gas_source = os.path.join(path_data, 'data_gasoline', 'data_source', 'data_other')
 ##df_insee.to_csv(os.path.join(path_dir_gas_source, 'data_insee_extract.csv')
 ##                float_format='%.3f', encoding='utf-8', index=False)
-#
-## Adding columns with aggregated data using groupby
-## check : http://bconnelly.net/2013/10/22/summarizing-data-in-python-with-pandas/
-#ls_columns_interest = ['P10_LOG', 'P10_MEN', 'P10_PMEN',
-#                       'P10_RP_VOIT1P', 'P10_RP_VOIT1', 'P10_RP_VOIT2P']
-#for column in ls_columns_interest:
-#  df_insee[column] = df_insee[column].apply(lambda x: float(x) if x else float('nan'))
-#df_uu_agg = df_insee.groupby('UU2010')[ls_columns_interest].sum()
-#df_insee = pd.merge(df_insee, df_uu_agg, left_on = "UU2010",
-#                    right_index = True, suffixes = ('_COM', '_UU'))
-#print '\nINSEE EXTRACT ENRICHED\n', df_insee.info() 
-#
-## http://pandas.pydata.org/pandas-docs/dev/groupby.html
-## todo: read # http://stackoverflow.com/questions/15262134/apply-different-functions-to-different-items-in-group-object-python-pandas
-#
-### If only one: series
-##se_uu_men = df_insee.groupby('UU2010')['P10_MEN'].sum()
-##df_uu_men = pd.Series(se_uu_men, name = 'P10_MEN_UU').reset_index()
-##df_insee = pd.merge(df_insee, df_uu_men, on = "UU2010")
