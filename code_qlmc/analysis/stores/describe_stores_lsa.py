df24311f1fb1f34e26257edf717704a3ddf9780b
etna <echamayou@gmail.com>
2014-09-17 00:57:43 +0200

    update lsa analysis and desc

--- code_qlmc/analysis/stores/describe_stores_lsa.py
+++ code_qlmc/analysis/stores/describe_stores_lsa.py
@@ -46,24 +46,11 @@ for field in [u'DATE ouv', u'DATE ferm', u'DATE réouv',
 
 print u'Stores in data (active or not): {0:5d}'.format(len(df_lsa))
 
-# Stores still operating (add for any date e.g. 01/01/2014)
-df_lsa_active = df_lsa[(pd.isnull(df_lsa[u'DATE ferm'])) |\
-                       ((~pd.isnull(df_lsa[u'DATE ferm'])) &\
-                        (~pd.isnull(df_lsa[u'DATE réouv'])))]
-print u'Stores still operating: {0:5d}'.format(len(df_lsa_active))
+# Create retail group variable
 
-# Stores no more operating
-df_lsa_inactive = df_lsa[(~pd.isnull(df_lsa[u'DATE ferm'])) &\
-                         (pd.isnull(df_lsa[u'DATE réouv']))]
-print u'Stores no more operating: {0:5d}'.format(len(df_lsa_inactive))
+#print df_lsa['Enseigne'].value_counts().to_string()
+#print df_lsa['Ex enseigne'].value_counts().to_string()
 
-# Describe variables of operating stores
-for field in ['Statut', 'Type', 'Enseigne', 'Ex enseigne']:
-  print u'\nValue counts for:', field
-  print u'-'*20
-  print df_lsa_active[field].value_counts()
-
-# Create variable for groups
 dict_groupes = {'MOUSQUETAIRES'      : ['INTERMARCHE SUPER',
                                         'NETTO',
                                         'LE DRIVE INTERMARCHE',
@@ -96,7 +83,9 @@ dict_groupes = {'MOUSQUETAIRES'      : ['INTERMARCHE SUPER',
                                         '8 A HUIT',
                                         'PROXI SUPER',
                                         'MARCHE PLUS',
-                                        'CARREFOUR MONTAGNE'], 
+                                        'CARREFOUR MONTAGNE',
+                                        'CHAMPION',
+                                        'HYPER CHAMPION'], 
                 'CASINO'             : ['LEADER PRICE',
                                         'FRANPRIX',
                                         'CASINO',
@@ -112,9 +101,15 @@ dict_groupes = {'MOUSQUETAIRES'      : ['INTERMARCHE SUPER',
                                         'MONOPRIX DRIVE',
                                         'CASINO SHOPPING',
                                         'LEADER EXPRESS',
-                                        'JUMBO SCORE',
+                                        'JUMBO SCORE', # check JUMBO SCORE
                                         'PETIT CASINO',
-                                        'LEADER MARKET'], # check JUMBO SCORE 
+                                        'LEADER MARKET',
+                                        'GEANT',
+                                        'CASINO EXPRESS',
+                                        'INNO',
+                                        'DISCOUNT CASINO',
+                                        "DAILYMONOP'",
+                                        'SUPER MONOPRIX'],  
                 'LECLERC'            : ['CENTRE E.LECLERC',
                                         'E.LECLERC DRIVE',
                                         'LECLERC EXPRESS'],
@@ -128,7 +123,8 @@ dict_groupes = {'MOUSQUETAIRES'      : ['INTERMARCHE SUPER',
                                         'A2PAS',
                                         'SIMPLY DRIVE',
                                         'EASY MARCHE',
-                                        'AUCHAN CITY'], 
+                                        'AUCHAN CITY',
+                                        'MAXIMARCHE'], 
                 'LOUIS DELHAIZE'     : ['SUPERMARCHE MATCH',
                                         'CORA',
                                         'CORADRIVE',
@@ -141,39 +137,168 @@ dict_groupes = {'MOUSQUETAIRES'      : ['INTERMARCHE SUPER',
                                         'COCCINELLE SUPERMARCHE',
                                         'COCCINELLE EXPRESS',
                                         'COCCIMARKET',
-                                        'COCCINELLE MARCHE']}
-dict_enseigne_groupe = {x: k for k,v in dict_groupes.items() for x in v}
+                                        'COCCINELLE MARCHE'],
+                'ALDI'                : ['ALDI'],
+                'LIDL'                : ['LIDL']}
 
 # MUTANT EXPRESS, CDM, AU MARCHE VRAC...
+
+dict_enseigne_groupe = {x: k for k,v in dict_groupes.items() for x in v}
+df_lsa['Groupe'] = df_lsa['Enseigne'].apply(\
+                     lambda x: dict_enseigne_groupe.get(x, 'AUTRE') if not pd.isnull(x)\
+                                                       else None)
+df_lsa['Ex groupe'] = df_lsa['Ex enseigne'].apply(\
+                     lambda x: dict_enseigne_groupe.get(x, 'AUTRE') if not pd.isnull(x)\
+                                                       else None)
+
+# Stores still operating (add for any date e.g. 01/01/2014)
+df_lsa_active = df_lsa[(pd.isnull(df_lsa[u'DATE ferm'])) |\
+                       ((~pd.isnull(df_lsa[u'DATE ferm'])) &\
+                        (~pd.isnull(df_lsa[u'DATE réouv'])))]
+print u'Stores still operating: {0:5d}'.format(len(df_lsa_active))
+
+# Stores no more operating
+df_lsa_inactive = df_lsa[(~pd.isnull(df_lsa[u'DATE ferm'])) &\
+                         (pd.isnull(df_lsa[u'DATE réouv']))]
+print u'Stores no more operating: {0:5d}'.format(len(df_lsa_inactive))
+
+# Describe variables of operating stores
+for field in ['Statut', 'Type', 'Enseigne', 'Ex enseigne', 'Groupe']:
+  print u'\nValue counts for:', field
+  print u'-'*20
+  print df_lsa_active[field].value_counts()
+
+# STATS BY TYPE
+pd.set_option('float_format', '{:10.0f}'.format)
+
+dict_type_out = {'S' : 'Supermarkets',
+                 'H' : 'Hypermarkets',
+                 'DRIN' : 'Drive in',
+                 'X' : 'Hard discount',
+                 'DRIVE' : 'Drive',
+                 'MP' : '"Magasins populaires'}
+
+for field in ['Surf Vente', 'Nbr de caisses', 'Nbr parking', 'Pompes']:
+  print '\n', field
+  gbt = df_lsa_active[['Type', field]].groupby('Type',
+                                                      as_index = False)
+  df_surf = gbt.agg([len, np.mean, np.median, np.min, np.max, np.sum])[field]
+  df_surf.sort('len', ascending = False, inplace = True)
+  se_null_vc = df_lsa_active['Type'][pd.isnull(df_lsa_active[field])].value_counts()
+  df_surf['#Null'] = se_null_vc
+  df_surf.rename(columns = {'len' : '#Tot',
+                            'mean': 'Avg',
+                            'median': 'Med',
+                            'amin': 'Min',
+                            'amax': 'Max',
+                            'sum': 'Cum'}, inplace = True)
+  ls_surf_disp = ['#Tot', '#Null', 'Avg', 'Med', 'Min', 'Max', 'Cum']
+  df_surf.reset_index(inplace = True)
+  df_surf['Type'] = df_surf['Type'].apply(lambda x: dict_type_out[x])
+  df_surf.set_index('Type', inplace = True)
+  print df_surf[ls_surf_disp].to_latex(index_names = False)
+
+# STATS ON NB OF STORES AND SURFACE BY RETAIL GROUPS/CHAINS
+
+# Stats desc by retail group
+print '\nStats des per retail group'
+df_lsa_gps = df_lsa_active[(df_lsa_active['Type'] != 'DRIN') &\
+                           (df_lsa_active['Type'] != 'DRIVE')]
+gbg = df_lsa_gps[['Groupe', 'Surf Vente']].groupby('Groupe',
+                                                   as_index = False)
+df_surf = gbg.agg([len, np.mean, np.median, np.min, np.max, np.sum])['Surf Vente']
+df_surf.sort('len', ascending = False, inplace = True)
+# print df_surf.to_string()
+df_types = pd.DataFrame(columns = ['H', 'S', 'X', 'MP']).T
+for groupe in df_lsa_gps['Groupe'].unique():
+  se_types_vc = df_lsa_gps['Type'][df_lsa_gps['Groupe'] == groupe].value_counts()
+  df_types[groupe] = se_types_vc
+df_types.fillna(0, inplace = True)
+df_types = df_types.T
+# print df_types.to_string()
+df_rgps = pd.merge(df_types, df_surf, left_index = True, right_index = True)
+df_rgps.sort('len', ascending = False, inplace = True)
+df_rgps.rename(columns = {'len' : '#Tot',
+                          'mean': 'Avg S.',
+                          'median': 'Med S.',
+                          'amin': 'Min S.',
+                          'amax': 'Max S.',
+                          'sum': 'Cum S.',
+                          'H' : '#Hyp',
+                          'S' : '#Sup',
+                          'X' : '#Dis',
+                          'MP': '#MP'}, inplace = True)
+ls_rgps_disp = ['#Tot', '#Hyp', '#Sup', '#Dis', '#MP',
+                'Avg S.', 'Med S.', 'Min S.', 'Max S.', 'Cum S.']
+print df_rgps[ls_rgps_disp].to_latex()
+
+# Stats desc by enseigne (and by group)
+print '\nStats des per retail group'
+for retail_group in dict_groupes.keys():
+  print '\n', retail_group
+  df_lsa_rgp = df_lsa_active[(df_lsa_active['Groupe'] == retail_group) &\
+                             (df_lsa_active['Type'] != 'DRIN') &\
+                             (df_lsa_active['Type'] != 'DRIVE')]
+  gbg = df_lsa_rgp[['Enseigne', 'Surf Vente']].groupby('Enseigne',
+                                                       as_index = False)
+  df_surf = gbg.agg([len, np.mean, np.median, np.min, np.max, np.sum])['Surf Vente']
+  df_surf.sort('len', ascending = False, inplace = True)
+  # print df_surf.to_string()
+  df_types = pd.DataFrame(columns = ['H', 'S', 'X', 'MP']).T
+  for enseigne in df_lsa_rgp['Enseigne'].unique():
+    se_types_vc = df_lsa_rgp['Type'][df_lsa_rgp['Enseigne'] == enseigne].value_counts()
+    df_types[enseigne] = se_types_vc
+  df_types.fillna(0, inplace = True)
+  df_types = df_types.T
+  # print df_types.to_string()
+  df_rgp = pd.merge(df_types, df_surf, left_index = True, right_index = True)
+  df_rgp.sort('len', ascending = False, inplace = True)
+  df_rgp.rename(columns = {'len' : '#Tot',
+                           'mean': 'Avg S.',
+                           'median': 'Med S.',
+                           'amin': 'Min S.',
+                           'amax': 'Max S.',
+                           'sum': 'Cum S.',
+                           'H' : '#Hyp',
+                           'S' : '#Sup',
+                           'X' : '#Dis',
+                           'MP': '#MP'}, inplace = True)
+  ls_rgp_disp = ['#Tot', '#Hyp', '#Sup', '#Dis', '#MP',
+                 'Avg S.', 'Med S.', 'Min S.', 'Max S.', 'Cum S.']
+  print df_rgp[ls_rgp_disp].to_latex()
 
 #print u'\nNb stores (dead or alive) with valid gps:'
 #print len(df_lsa[(~pd.isnull(df_lsa['Longitude'])) &\
 #                 (~pd.isnull(df_lsa['Latitude']))])
 #
 
-# STORES OPENED WITHIN QLMC DATA PERIOD
+# OUTPUT FOR MAP CREATION
+df_lsa_active.to_csv(os.path.join(path_dir_built_csv, 'df_lsa_active.csv'),
+                     encoding = 'UTF-8', , float_format='%.3f')
+
+# INTER RETAIL GROUP BRAND CHANGES (ACTIVE STORES)
+
 ls_disp_1 = [u'Ident', u'Enseigne', u'ADRESSE1', u'Ville', 'Code INSEE',
              u'DATE ouv', u'DATE ferm', u'DATE réouv']
 
-print u'\nStores with "significant" brand changes over qlmc period'
-df_lsa_chge = df_lsa.copy()
-df_lsa_chge = df_lsa_chge[~((df_lsa_chge['Ex enseigne'].str.contains('CHAMPION') |\
-                             df_lsa_chge['Ex enseigne'].str.contains('SHOPI')) &\
-                            (df_lsa_chge['Enseigne'].str.contains('CARREFOUR')))]
-df_lsa_chge = df_lsa_chge[~((df_lsa_chge['Ex enseigne'].str.contains('INTERMARCHE') |\
-                             df_lsa_chge['Ex enseigne'].str.contains('ECOMARCHE')) &\
-                            (df_lsa_chge['Enseigne'].str.contains('INTERMARCHE')))]
-df_lsa_chge = df_lsa_chge[~((df_lsa_chge['Ex enseigne'].str.contains('GEANT') |\
-                             df_lsa_chge['Ex enseigne'].str.contains('CASINO')) &\
-                            (df_lsa_chge['Enseigne'].str.contains('CASINO')))]
-# not full: check with regular expression
-df_lsa_chge = df_lsa_chge[~((df_lsa_chge['Ex enseigne'].str.contains('MARCHE U') |\
-                             df_lsa_chge['Ex enseigne'].str.contains('SUPER U')) &\
-                            (df_lsa_chge['Enseigne'].str.contains('U EXPRESS')))]
-#print df_lsa_chge[ls_disp_2].to_string()
+ls_disp_2 = [u'Ident', u'Enseigne', u'Ex enseigne', u'DATE chg enseigne', u'Ville', 'Code INSEE',
+             u'DATE ouv', u'DATE ferm', u'DATE réouv']
 
-# todo: detect duplicates
-# SIRET/SIREN
+print u'\nStores with retail group change'
+df_lsa_chge = df_lsa_active[(~pd.isnull(df_lsa_active['Ex groupe'])) &\
+                            (df_lsa_active['Groupe'] != df_lsa_active['Ex groupe'])]
+print u'Stores in groupe chge (incl. AUTRE): {0:5d}'.format(len(df_lsa_chge))
+
+df_lsa_chge_2 = df_lsa_active[(~pd.isnull(df_lsa_active['Ex groupe'])) &\
+                              (df_lsa_active['Ex groupe'] != 'AUTRE') &\
+                              (df_lsa_active['Groupe'] != 'AUTRE') &\
+                              (df_lsa_active['Groupe'] != df_lsa_active['Ex groupe'])]
+print u'Stores in groupe chge (no AUTRE): {0:5d}'.format(len(df_lsa_chge_2))
+
+#print df_lsa_chge[ls_disp_2].to_string()
+#print df_lsa_chge_2[ls_disp_2].to_string()
+
+# SIRET/SIREN: duplicates
 df_lsa[u'N°Siren'] = df_lsa[u'N°Siren'].apply(\
                        lambda x: u'{:09d}'.format(int(x)) if not pd.isnull(x)\
                                                           else None)
@@ -187,9 +312,9 @@ df_lsa[u'dup'] = se_siret_vc
 
 df_dup = df_lsa[df_lsa['dup'] == 2].copy()
 df_dup.sort(inplace = True)
-print df_dup[ls_disp_1].to_string()
+#print df_dup[ls_disp_1].to_string()
 
-# generate duplicate detection:
+# GENERA DUPLICATE DETECTION
 # loop on closed store... search newly opened stores within same area (or same siret btw)
 df_lsa_closed_a = df_lsa[(~pd.isnull(df_lsa[u'DATE ferm'])) &\
                          (pd.isnull(df_lsa[u'DATE réouv']))].copy()
@@ -205,14 +330,16 @@ for i, row_i in df_lsa_closed_a.iterrows():
   # print '\nFermeture:', row_i[u'DATE ferm']
   ls_open = []
   for j, row_j in df_lsa[df_lsa[u'Code INSEE'] == row_i['Code INSEE']].iterrows():
-    td = row_j[u'DATE ouv'] - row_i[u'DATE ferm']
-    # could drop second condition
-    if (td >= datetime.timedelta(days=0)) and\
-       (td <= datetime.timedelta(days=365)):
-      #print 'Ouverture, :', row_i[u'DATE ouv'], 'Time delta:', td
-      #print row_i[u'Enseigne'], row_i['ADRESSE1'], row_i['Siret']
-      #print row_j[u'Enseigne'], row_j['ADRESSE1'], row_j['Siret']
-      ls_open.append([row_j['Ident'], td])
+    # crashes if DATE ouv is NaT (how many?)
+    if not pd.isnull(row_j[u'DATE ouv']):
+      td = row_j[u'DATE ouv'] - row_i[u'DATE ferm']
+      # could drop second condition
+      if (td >= datetime.timedelta(days=0)) and\
+         (td <= datetime.timedelta(days=365)):
+        #print 'Ouverture, :', row_i[u'DATE ouv'], 'Time delta:', td
+        #print row_i[u'Enseigne'], row_i['ADRESSE1'], row_i['Siret']
+        #print row_j[u'Enseigne'], row_j['ADRESSE1'], row_j['Siret']
+        ls_open.append([row_j['Ident'], td])
   ls_suspects.append([row_i['Ident'], ls_open])
 
 # Check chains and if stores closed after reconciliation
@@ -229,47 +356,22 @@ for row in ls_suspects:
 
 # df_lsa[ls_disp_1].ix[[20, 124704]]
 
-print u'\nStores with multiple lines'
-print df_lsa[ls_disp_1].ix[ls_s_noclosed_ind].to_string()
-
-print u'\nStores potentially closed and not replaced'
-print df_lsa[ls_disp_1].ix[ls_s_closed_ind].to_string()
-
-# Inspect: new opening very same day
-# Inspect: new opening later on (any date...)
-
-# Check these weird lines
-print u'\nWeird lines: opened and closed on same date'
-print df_lsa[ls_disp_1][df_lsa['DATE ouv'] == df_lsa['DATE ferm']].to_string()
+#print u'\nStores with multiple lines'
+#print df_lsa[ls_disp_1].ix[ls_s_noclosed_ind].to_string()
+#
+#print u'\nStores potentially closed and not replaced'
+#print df_lsa[ls_disp_1].ix[ls_s_closed_ind].to_string()
+#
+## Inspect: new opening very same day
+## Inspect: new opening later on (any date...)
+#
+## Check these weird lines
+#print u'\nWeird lines: opened and closed on same date'
+#print df_lsa[ls_disp_1][df_lsa['DATE ouv'] == df_lsa['DATE ferm']].to_string()
 
 # todo: increase in store surface?
 # todo: gps quality? missing data? distances? (google API)
 
-## GENERAT COLUMNS BY PERIOD WITH CURRENT ENSEIGNE WHEN STORE IS ACTIVE
-#ls_qlmc_dates = ['2007-05',
-#                 '2007-08',
-#                 '2008-01',
-#                 '2008-04',
-#                 '2009-03',
-#                 '2009-09',
-#                 '2010-03',
-#                 '2010-10',
-#                 '2011-01',
-#                 '2011-04',
-#                 '2011-10',
-#                 '2012-01',
-#                 '2012-06']
-#
-#for qlmc_date in ls_qlmc_dates:
-#  df_lsa[qlmc_date] = df_lsa['Enseigne']
-#  # Not opened yet (left untouched if DATE ouv is unknown)
-#  df_lsa[qlmc_date][df_lsa[u'DATE ouv'] > qlmc_date] = None
-#  # Closed and not reopened
-#  df_lsa[qlmc_date][(df_lsa[u'DATE ferm'] < qlmc_date) &
-#                    (pd.isnull(df_lsa[u'DATE réouv']))] = None
-#  # Write previous enseigne if changed since then (check consistency if missing data)
-#  df_lsa[qlmc_date][(qlmc_date > df_lsa[u'DATE ouv']) &\
-#                    (qlmc_date < df_lsa[u'DATE chg enseigne'])] = df_lsa['Ex enseigne']
 #
 ### FORMAT DATE AND OUTPUT TO EXCEL FILE
 ##for field in [u'DATE ouv', u'DATE ferm', u'DATE réouv',
@@ -283,9 +385,7 @@ print df_lsa[ls_disp_1][df_lsa['DATE ouv'] == df_lsa['DATE ferm']].to_string()
 ##df_lsa.to_excel(writer, index = False)
 ##writer.close()
 
-# TODO: get ARDT for big cities thx to gps
-
-## qlmc_data = pd.HDFStore(os.path.join(path_dir_built_hdf5, 'qlmc_data.h5'))
+# todo: get ARDT for big cities thx to gps
 
 ## INTEGRATE INSEE DATA FOR ANALYSIS
 #df_insee_areas = pd.read_csv(os.path.join(path_dir_insee_extracts, 'df_insee_areas.csv'))
