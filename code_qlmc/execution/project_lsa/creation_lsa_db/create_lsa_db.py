6533248671ad68242ce830bd9e648ac3cf01dd88
etiennecha <echamayou@gmail.com>
2014-10-02 17:06:28 +0200

    update lsa tables and report

--- code_qlmc/execution/project_lsa/creation_lsa_db/create_lsa_db.py
+++ code_qlmc/execution/project_lsa/creation_lsa_db/create_lsa_db.py
@@ -249,6 +249,9 @@ for field in ['Statut', 'Type_alt', 'Enseigne_alt', 'Ex enseigne', 'Groupe']:
 # STATS BY TYPE OF STORE 
 # ######################
 
+format_float_int = lambda x: '{:10,.0f}'.format(x)
+format_float_float = lambda x: '{:10,.2f}'.format(x)
+
 #df_lsa['Pompes'][pd.isnull(df_lsa['Pompes'])] = np.nan
 # solve pbm: why min and max on grouby return nan...
 
@@ -382,88 +385,147 @@ ls_ind_disp = list(df_rgs.index[1:])
 ls_ind_disp.remove('AUTRE')
 print df_rgs[ls_rgs_disp].loc[ls_ind_disp + ['AUTRE', 'ALL']].to_latex()
 
-
 # BY RETAIL GROUP 2 (NEW)
-ls_columns = ['Nb.', 'Cum. Surf.'] + 3*['Hyp', 'Sup', 'Disc.']
+
+ls_stats = ['Nb_', 'Cum_S_', 'Avg_S_']
+ls_store_types = ['H', 'S', 'X']
+ls_columns = [''.join((y,x)) for x in ls_store_types for y in ls_stats]
+# Main loop
 ls_rows_rg = []
 for retail_group in df_lsa_gps['Groupe'].unique():
   df_lsa_rg = df_lsa_gps[df_lsa_gps['Groupe'] == retail_group]
-  nb_stores = float(len(df_lsa_rg))
-  nb_h = float(len(df_lsa_rg[df_lsa_rg['Type_alt'] == 'H']))
-  nb_s = float(len(df_lsa_rg[df_lsa_rg['Type_alt'] == 'S']))
-  nb_x = float(len(df_lsa_rg[df_lsa_rg['Type_alt'] == 'X']))
-  avg_h = df_lsa_rg['Surf Vente'][df_lsa_rg['Type_alt'] == 'H'].mean()
-  avg_s = df_lsa_rg['Surf Vente'][df_lsa_rg['Type_alt'] == 'S'].mean()
-  avg_x = df_lsa_rg['Surf Vente'][df_lsa_rg['Type_alt'] == 'X'].mean()
-  cum_h = df_lsa_rg['Surf Vente'][df_lsa_rg['Type_alt'] == 'H'].sum()
-  cum_s = df_lsa_rg['Surf Vente'][df_lsa_rg['Type_alt'] == 'S'].sum()
-  cum_x = df_lsa_rg['Surf Vente'][df_lsa_rg['Type_alt'] == 'X'].sum()
-  ls_rows_rg.append([nb_stores, cum_h + cum_s + cum_x,
-                     nb_h, nb_s, nb_x,
-                     avg_h, avg_s, avg_x,
-                     cum_h, cum_s, cum_x])
+  row = []
+  for st_type in ls_store_types:
+    nb_stores = float(len(df_lsa_rg[df_lsa_rg['Type_alt'] == st_type]))
+    cum_surf = df_lsa_rg['Surf Vente'][df_lsa_rg['Type_alt'] == st_type].sum() / 1000000
+    avg_surf = df_lsa_rg['Surf Vente'][df_lsa_rg['Type_alt'] == st_type].mean()
+    row += [nb_stores, cum_surf, avg_surf]
+  ls_rows_rg.append(row)
 # Bottom row
-nb_stores = float(len(df_lsa_gps))
-nb_h = float(len(df_lsa_gps[df_lsa_gps['Type_alt'] == 'H']))
-nb_s = float(len(df_lsa_gps[df_lsa_gps['Type_alt'] == 'S']))
-nb_x = float(len(df_lsa_gps[df_lsa_gps['Type_alt'] == 'X']))
-avg_h = df_lsa_gps['Surf Vente'][df_lsa_gps['Type_alt'] == 'H'].mean()
-avg_s = df_lsa_gps['Surf Vente'][df_lsa_gps['Type_alt'] == 'S'].mean()
-avg_x = df_lsa_gps['Surf Vente'][df_lsa_gps['Type_alt'] == 'X'].mean()
-cum_h = df_lsa_gps['Surf Vente'][df_lsa_gps['Type_alt'] == 'H'].sum()
-cum_s = df_lsa_gps['Surf Vente'][df_lsa_gps['Type_alt'] == 'S'].sum()
-cum_x = df_lsa_gps['Surf Vente'][df_lsa_gps['Type_alt'] == 'X'].sum()
-ls_rows_rg.append([nb_stores, cum_h + cum_s + cum_x,
-                   nb_h, nb_s, nb_x,
-                   avg_h, avg_s, avg_x,
-                   cum_h, cum_s, cum_x])
+row = []
+for st_type in ls_store_types:
+  nb_stores = float(len(df_lsa_gps[df_lsa_gps['Type_alt'] == st_type]))
+  cum_surf = df_lsa_gps['Surf Vente'][df_lsa_gps['Type_alt'] == st_type].sum() / 1000000
+  avg_surf = df_lsa_gps['Surf Vente'][df_lsa_gps['Type_alt'] == st_type].mean()
+  row += [nb_stores, cum_surf, avg_surf]
+ls_rows_rg.append(row)
+
 df_rgs_2 = pd.DataFrame(ls_rows_rg,
                         columns = ls_columns,
                         index = list(df_lsa_gps['Groupe'].unique()) + ['ALL'])
-df_rgs_2.sort('Nb.', ascending = False, inplace = True)
-ls_ind_disp = list(df_rgs.index[1:])
-ls_ind_disp.remove('AUTRE')
-print df_rgs_2.loc[ls_ind_disp + ['AUTRE', 'ALL']].to_latex()
-# {} & \multicolumn{2}{c|}{All} &   \multicolumn{3}{c|}{Nb.} &
-# \multicolumn{3}{c|}{Avg. Surf.}   &   \multicolumn{3}{c}{Cum. Surf.} \\
+# Add Nb total and Cum surf
+df_rgs_2['Nb_All'] = df_rgs_2[['Nb_H', 'Nb_S', 'Nb_X']].sum(axis = 1)
+df_rgs_2['Cum_S_All'] = df_rgs_2[['Cum_S_H', 'Cum_S_S', 'Cum_S_X']].sum(axis = 1)
+df_rgs_2.sort('Nb_All', ascending = False, inplace = True)
+# Add percentages
+df_rgs_2_pc = df_rgs_2.copy()
+df_rgs_2_pc[['Nb_H', 'Nb_S', 'Nb_X', 'Nb_All']] =\
+   df_rgs_2_pc[['Nb_H', 'Nb_S', 'Nb_X', 'Nb_All']].apply(\
+     lambda x: x * 100 / x['Nb_All'], axis = 1)
+df_rgs_2_pc[['Cum_S_H', 'Cum_S_S', 'Cum_S_X', 'Cum_S_All']] =\
+   df_rgs_2_pc[['Cum_S_H', 'Cum_S_S', 'Cum_S_X', 'Cum_S_All']].apply(\
+     lambda x: x * 100 / x['Cum_S_All'], axis = 1)
+df_rgs_2_pc[['Avg_S_H', 'Avg_S_S', 'Avg_S_X']] = np.nan
 
+ls_col_disp = ['Nb_All', 'Nb_H', 'Nb_S', 'Nb_X',
+               'Cum_S_All', 'Cum_S_H', 'Cum_S_S', 'Cum_S_X',
+               'Avg_S_H', 'Avg_S_S', 'Avg_S_X']
+
+ls_row_disp = list(df_rgs.index[1:])
+ls_row_disp.remove('AUTRE')
+ls_row_disp += ['AUTRE', 'ALL']
+
+dict_formatters = {'Cum_S_All' : format_float_float,
+                   'Cum_S_H' : format_float_float,
+                   'Cum_S_S' : format_float_float,
+                   'Cum_S_X' : format_float_float}
+
+## Pbm: dirty 100.00 hence rather merge string after latex ouput (see italic?)
+#ls_rows_rgs_fin = []
+#for x in ls_row_disp:
+#  ls_rows_rgs_fin.append(df_rgs_2[ls_col_disp].loc[x])
+#  ls_rows_rgs_fin.append(df_rgs_2_pc[ls_col_disp].loc[x])
+#df_rgs_2_fin = pd.concat(ls_rows_rgs_fin, axis = 1).T
+#print df_rgs_2_fin[ls_col_disp].to_string(formatters = dict_formatters)
+
+latex_rgs_2 = u''
+for (x,y) in zip(df_rgs_2[ls_col_disp].loc[ls_row_disp].\
+                   to_latex(formatters = dict_formatters).\
+                     replace('nan', '   ').split('\n')[4:-2],
+                 df_rgs_2_pc[ls_col_disp].loc[ls_row_disp].\
+                   to_latex().replace('nan', '   ').split('\n')[4:-2]):
+  latex_rgs_2 += u'%s\n'%x
+  latex_rgs_2 += u'%s\n'%y
+print latex_rgs_2
+
+# TODO: add % of total: Nb and Surface
 
 # BY ENSEIGNE FOR EACH GROUP
 print '\nStats des per retail group'
+
+dict_rename_rg = {'len' : '#Tot',
+                  'mean': 'Avg S.',
+                  'median': 'Med S.',
+                  'min': 'Min S.',
+                  'max': 'Max S.',
+                  'sum': 'Cum S.',
+                  'H' : '#Hyp',
+                  'S' : '#Sup',
+                  'X' : '#Dis'}
+
+ls_rg_disp = ['#Tot', '#Hyp', '#Sup', '#Dis',
+               'Avg S.', 'Med S.', 'Min S.', 'Max S.', 'Cum S.']
+
+dict_formatters = {'Cum S.' : format_float_float}
+
 for retail_group in dict_groupes.keys():
   print '\n', retail_group
+  
+  # All group stores
   df_lsa_rg = df_lsa_gps[df_lsa_gps['Groupe'] == retail_group]
   gbg = df_lsa_rg[['Enseigne_alt', 'Surf Vente']].groupby('Enseigne_alt',
                                                        as_index = False)
   df_surf_rg = gbg.agg([len, np.mean, np.median, min, max, np.sum])['Surf Vente']
   df_surf_rg.sort('len', ascending = False, inplace = True)
-  # print df_surf.to_string()
+  # print df_surf_rg.to_string()
   df_types_rg = pd.DataFrame(columns = ['H', 'S', 'X']).T
   for enseigne in df_lsa_rg['Enseigne_alt'].unique():
-    se_types_vc = df_lsa_rg['Type_alt'][df_lsa_rg['Enseigne_alt'] == enseigne].value_counts()
+    se_types_vc = df_lsa_rg['Type_alt'][df_lsa_rg['Enseigne_alt'] ==\
+                                          enseigne].value_counts()
     df_types_rg[enseigne] = se_types_vc
   df_types_rg.fillna(0, inplace = True)
   df_types_rg = df_types_rg.T
-  # print df_types.to_string()
+  # print df_types_rg.to_string()
   df_rg = pd.merge(df_types_rg, df_surf_rg, left_index = True, right_index = True)
   df_rg.sort('len', ascending = False, inplace = True)
-  df_rg.rename(columns = {'len' : '#Tot',
-                           'mean': 'Avg S.',
-                           'median': 'Med S.',
-                           'min': 'Min S.',
-                           'max': 'Max S.',
-                           'sum': 'Cum S.',
-                           'H' : '#Hyp',
-                           'S' : '#Sup',
-                           'X' : '#Dis'}, inplace = True)
-  ls_rg_disp = ['#Tot', '#Hyp', '#Sup', '#Dis',
-                 'Avg S.', 'Med S.', 'Min S.', 'Max S.', 'Cum S.']
-  print df_rg[ls_rg_disp].to_latex()
-
-#print u'\nNb stores (dead or alive) with valid gps:'
-#print len(df_lsa[(~pd.isnull(df_lsa['Longitude'])) &\
-#                 (~pd.isnull(df_lsa['Latitude']))])
-#
+  df_rg.rename(columns = dict_rename_rg, inplace = True)
+  df_rg['Cum S.'] = df_rg['Cum S.'] / 1000000
+  print df_rg[ls_rg_disp].to_latex(formatters = dict_formatters)
+  
+  # Independent stores
+  df_lsa_rg_ind = df_lsa_gps[(df_lsa_gps['Groupe'] == retail_group) &
+                             (df_lsa_gps[u'Intégré / Indépendant'] == 'independant')]
+  if len(df_lsa_rg_ind) != 0:
+    gbg = df_lsa_rg_ind[['Enseigne_alt', 'Surf Vente']].groupby('Enseigne_alt',
+                                                             as_index = False)
+    df_surf_rg_ind = gbg.agg([len, np.mean, np.median, min, max, np.sum])['Surf Vente']
+    df_surf_rg_ind.sort('len', ascending = False, inplace = True)
+    # print df_surf_rg_ind.to_string()
+    df_types_rg_ind = pd.DataFrame(columns = ['H', 'S', 'X']).T
+    for enseigne in df_lsa_rg_ind['Enseigne_alt'].unique():
+      se_types_vc = df_lsa_rg_ind['Type_alt'][df_lsa_rg_ind['Enseigne_alt'] ==\
+                                                enseigne].value_counts()
+      df_types_rg_ind[enseigne] = se_types_vc
+    df_types_rg_ind.fillna(0, inplace = True)
+    df_types_rg_ind = df_types_rg_ind.T
+    # print df_types.to_string()
+    df_rg_ind = pd.merge(df_types_rg_ind, df_surf_rg_ind, left_index = True, right_index = True)
+    df_rg_ind.sort('len', ascending = False, inplace = True)
+    df_rg_ind.rename(columns = dict_rename_rg, inplace = True)
+    df_rg_ind['Cum S.'] = df_rg_ind['Cum S.'] / 1000000
+    print df_rg_ind[ls_rg_disp].to_latex(formatters = dict_formatters)
+  else:
+    print 'No indpt store'
 
 # #########################################
 # STATS ON NB OF STORES BY GROUP AND REGION
@@ -597,8 +659,11 @@ ls_disp = ['CR1_n' , 'CR2_n', 'CR3_n', 'CR4_n',
            'CR1_s' , 'CR2_s', 'CR3_s', 'CR4_s',
            'HHI_n', 'HHI_s']
 
+dict_formatters = {'HHI_n' : format_float_float,
+                   'HHI_s' : format_float_float}
+
 # pbm: need to precise display for HHI columns + pbms with small brands
-print df_reg_cr[ls_disp].to_string()
+print df_reg_cr[ls_disp].to_latex(formatters = dict_formatters)
 
 ## #######################
 ## OUTPUT FOR MAP CREATION
