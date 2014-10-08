c41f24bf19eaf5083e5e1c471a7d94110346ddac
etna <echamayou@gmail.com>
2014-10-08 08:45:54 +0200

    update ameli analysis

--- code_ameli/execution/analysis_ameli_db/analyze_ameli_gp_2014.py
+++ code_ameli/execution/analysis_ameli_db/analyze_ameli_gp_2014.py
@@ -6,77 +6,69 @@ from add_to_path import path_data
 from generic_ameli import *
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-#path_dir_built_hdf5 = os.path.join(path_dir_built, 'hdf5')
+
+path_dir_ameli = os.path.join(path_data, u'data_ameli', 'data_source', 'ameli_2014')
+path_dir_built_csv= os.path.join(path_data, u'data_ameli', 'data_built', 'csv')
+path_dir_built_json = os.path.join(path_data, u'data_ameli', 'data_built', 'json')
+#path_dir_built_hdf5 = os.path.join(path_data, u'data_ameli', 'data_built', 'hdf5')
 #ameli_data = pd.HDFStore(os.path.join(path_dir_built_hdf5, 'ameli_data.h5'))
 
-path_dir_built_json = os.path.join(path_data, u'data_ameli', 'data_built', 'json')
-file_extension = u'ophtalmologiste_75'
-ls_ls_physicians = dec_json(os.path.join(path_dir_built_json, '%s.json' %file_extension))
+df_physicians = pd.read_csv(os.path.join(path_dir_built_csv, 'gp_2014.csv'),
+                            encoding = 'utf-8')
 
-ls_columns = ['id_physician', 'gender', 'name', 'surname',
-              'street', 'zip_city', 'convention', 'carte_vitale', 'status',
-              'injection_med', 'examen_mot', 'imagerie', 'traitement_las',
-              'fond', 'examen_vis', 'chirurgie_cat', 'consultation', 'avis']
-df_physicians = pd.DataFrame(ls_ls_physicians, columns = ls_columns)
-df_physicians.index = df_physicians['id_physician']
+# TODO: add INSEE data
 
-# DISPLAY DATA
-ls_disp_base = ['gender','name', 'surname', 'street', 'zip_city',
-                'convention', 'carte_vitale', 'status']
-ls_disp_services = ['consultation', 'avis', 'fond', 'imagerie', 'chirurgie_cat']
+# Stats descs
+pd.set_option('float_format', '{:.1f}'.format)
 
-# print df_physicians[ls_disp_base + ls_disp_services].to_string()
-print df_physicians[ls_disp_base + ls_disp_services]\
-        [pd.isnull(df_physicians['consultation'])].to_string()
+# All
+gb_zc = df_physicians[['zip_city', 'c_base']].groupby('zip_city')
+df_zc = gb_zc.agg([len, np.mean, np.median])['c_base']
+df_zc['len'] = df_zc['len'].astype(int)
+print df_zc.to_string()
 
-## COMPANY CREATION DATE
-#dict_results = dec_json(os.path.join(path_dir_ameli, u'societe_dict_ophtalmo'))
-#ls_no_result = dec_json(os.path.join(path_dir_ameli, u'societe_ls_no_result_ophtalmo'))
-#
-#dict_ste_physicians = {}
-#for id_physician, info_physician in dict_results.items():
-#  if u'médecin' in info_physician[2][0]:
-#    dict_ste_physicians[id_physician] = info_physician[:2] +\
-#                                        [[elt.strip() if elt else elt\
-#                                           for elt in info_physician[3]]]
-#  else:
-#    dict_results[id_physician][2] =\
-#      [elt.strip() if elt else elt\
-#        for elt in info_physician[2][0].replace('\n', '').split(';')]
-#    dict_results[id_physician][3] =\
-#      [elt.strip() if elt else elt for elt in info_physician[3]]
-#    print '\n', id_physician
-#    pprint.pprint(dict_results[id_physician])
-#
-#df_physicians['immatriculation'] = None
-#for id_physician, info_physician in dict_ste_physicians.items():
-#  len_info_ste = len(info_physician[2])
-#  ls_titles = [info_physician[2][i] for i in range(len_info_ste) if i%2 == 0]
-#  ls_contents = [info_physician[2][i] for i in range(len_info_ste) if i%2 != 0]
-#  dict_info_ste = dict(zip(ls_titles, ls_contents))
-#  dict_ste_physicians[id_physician][2] = dict_info_ste 
-#  # print id_physician, dict_info_ste.get('Immatriculation')
-#  df_physicians.ix[id_physician, 'immatriculation'] = dict_info_ste.get('Immatriculation')
-#df_physicians['immatriculation'] =\
-#  df_physicians['immatriculation'].apply(lambda x: float(x[-4:]) if x else None)
-#
-##Stats desc:
-##df_temp[['convention', 'Consultation']].groupby('convention').agg([len, np.mean])
+# todo: density rather than nb of physicians
+plt.scatter(df_zc_s2['len'], df_zc_s2['mean'])
+plt.show()
+
+# Secteur 2 (reproduce with min and max?)
+df_physicians_s2 = df_physicians[df_physicians['convention'] == '2'].copy()
+gb_zc_s2 = df_physicians_s2[['zip_city', 'c_base']].groupby('zip_city')
+df_zc_s2 = gb_zc_s2.agg([len, np.mean, np.median])['c_base']
+df_zc_s2['len'] = df_zc_s2['len'].astype(int)
+print df_zc_s2.to_string()
+
+# todo: density rather than nb of physicians
+plt.scatter(df_zc_s2['len'], df_zc_s2['mean'])
+plt.show()
+
 #df_cross = df_physicians[['convention','immatriculation','Consultation']].\
-#             groupby(['convention','immatriculation'])
-#df_cross = df_cross.count()['Consultation'].unstack(0)
-#df_cross_cum = df_cross.cumsum()
-#df_cross_cum_graph = df_cross_cum.fillna(method='bfill').fillna(method='pad')
-#
-## todo: clean
-## Some info wrong: 1900 => 2000 after verification with other site
-#df_physicians['immatriculation'][pd_df_physicians['immatriculation'] == 1900] = 2000
-## Misses 5
 #df_physicians['zip_city'][df_physicians['zip_city'].str.startswith('75116')] = '75016 PARIS'
 #df_temp = df_physicians[~df_physicians['zip_city'].str.contains('CEDEX')]
 #formula = 'Consultation ~ C(zip_city) + immatriculation + C(convention)'
 #res01 = smf.ols(formula = formula, data = df_temp, missing= 'drop').fit()
 ## todo: represent homogeneity within ardts
+
+# JSON DEPRECATED?
+
+#path_dir_built_json = os.path.join(path_data, u'data_ameli', 'data_built', 'json')
+#file_extension = u'ophtalmologiste_75'
+#ls_ls_physicians = dec_json(os.path.join(path_dir_built_json, '%s.json' %file_extension))
 #
-## CHECK OSM ROUTE SERVICE : http://wiki.openstreetmap.org/wiki/OpenRouteService
+#ls_columns = ['id_physician', 'gender', 'name', 'surname',
+#              'street', 'zip_city', 'convention', 'carte_vitale', 'status',
+#              'injection_med', 'examen_mot', 'imagerie', 'traitement_las',
+#              'fond', 'examen_vis', 'chirurgie_cat', 'consultation', 'avis']
+#df_physicians = pd.DataFrame(ls_ls_physicians, columns = ls_columns)
+#df_physicians.index = df_physicians['id_physician']
+#
+## DISPLAY DATA
+#ls_disp_base = ['gender','name', 'surname', 'street', 'zip_city',
+#                'convention', 'carte_vitale', 'status']
+#ls_disp_services = ['consultation', 'avis', 'fond', 'imagerie', 'chirurgie_cat']
+#
+## print df_physicians[ls_disp_base + ls_disp_services].to_string()
+#print df_physicians[ls_disp_base + ls_disp_services]\
+#        [pd.isnull(df_physicians['consultation'])].to_string()
--- code_ameli/execution/creation_ameli_db/read_ameli_gp_2014.py
+++ code_ameli/execution/creation_ameli_db/read_ameli_gp_2014.py
@@ -15,10 +15,10 @@ import statsmodels as sm
 import statsmodels.formula.api as smf
 
 path_dir_ameli = os.path.join(path_data, u'data_ameli', 'data_source', 'ameli_2014')
+path_dir_built_csv= os.path.join(path_data, u'data_ameli', 'data_built', 'csv')
 path_dir_built_json = os.path.join(path_data, u'data_ameli', 'data_built', 'json')
-
-# file_extension = u'medecin-generaliste_75001'
-#ls_ls_physicians = dec_json(os.path.join(path_dir_ameli, u'ls_ls_%s' %file_extension))
+#path_dir_built_hdf5 = os.path.join(path_data, u'data_ameli', 'data_built', 'hdf5')
+#ameli_data = pd.HDFStore(os.path.join(path_dir_built_hdf5, 'ameli_data.h5'))
 
 dict_physicians = {}
 for i in range(1, 21):
@@ -160,6 +160,7 @@ for id_physician, ls_physician in dict_physicians.items():
   ls_consultation_prices = [None, None, None, None]
   ls_other_prices = [None for i in ls_unique_services]
   for service in ls_physician[3]:
+    # todo: generalize
     if service[0] == u'Consultation':
       ls_consultation_prices = get_service_price(id_physician,
                                                  [ls_x[-1] for ls_x in service[2]])
@@ -290,6 +291,13 @@ ls_disp_services = ['c_base', 'c_proba', 'c_min', 'c_max',
 
 # STORE
 
+# CSV
+df_physicians[ls_disp_base_1 + ls_disp_services[:4]].\
+  to_csv(os.path.join(path_dir_built_csv, 'gp_2014.csv'),
+         encoding = u'utf-8',
+         float_format = u'%.1f')
+
+# JSON
 df_physicians = df_physicians[ls_disp_base_1 + ls_disp_services].copy()
 df_physicians.reset_index(inplace = True)
 ls_ls_physicians = [list(x) for x in df_physicians.values]
