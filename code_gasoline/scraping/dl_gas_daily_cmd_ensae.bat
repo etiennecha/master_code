4d7e7702dbb556add9736a71800d0e54d105fc8a
etna <echamayou@gmail.com>
2014-10-15 10:08:35 +0200

    beg of gasoline restructuration

--- code_gasoline/scraping/dl_gas_daily_cmd_ensae.bat
+++
@@ -1,1 +0,0 @@
-\\ulysse\users\echamayou\Etienne\Python\Ppython\App\python.exe \\ulysse\users\echamayou\Bureau\Etienne_work\Code\code_gasoline\scraping\dl_gas_daily.py >> \\ulysse\users\echamayou\Bureau\Etienne_work\Data\data_gasoline\data_source\data_prices\current_prices\log.txt
