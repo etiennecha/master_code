aae192d42ed5ab309ad8ddc22feade2429c9777e
etiennecha <echamayou@gmail.com>
2015-02-15 00:20:01 +0100

    update reg by station

--- code_gasoline/execution_total_access/analysis_db/impact_competition/reg_ta_comp_one_reg_by_station.py
+++ code_gasoline/execution_total_access/analysis_db/impact_competition/reg_ta_comp_one_reg_by_station.py
@@ -46,7 +46,8 @@ df_info_ta = pd.read_csv(os.path.join(path_dir_built_csv,
                                   'ci_ardt_2' : str,
                                   'dpt' : str},
                          parse_dates = [u'day_%s' %i for i in range(4)] +\
-                                       ['pp_chge_date', 'ta_chge_date'])
+                                       ['pp_chge_date', 'ta_chge_date',
+                                        'date_beg', 'date_end'])
 df_info_ta.set_index('id_station', inplace = True)
 
 df_info = pd.read_csv(os.path.join(path_dir_built_csv,
@@ -108,90 +109,97 @@ for id_station, ls_ta_comp in dict_ls_ta_comp.items():
       (ls_ta_comp[0][1] > 10)):
     ls_control_ids.append(id_station)
 
-# ##########################################
-# LONG PANEL: COMP OF TOTAL => TOTAL ACCESS
-# ##########################################
+# ##########
+# PARAMETERS
+# ##########
 
 df_prices = df_prices_ttc
 
-ls_ta_chge_ids = list(df_info_ta.index[(df_info_ta['pp_chge'] >= 0.05) &\
-                                       (~pd.isnull(df_info_ta['date_beg']))])
-
-#df_tta_lg_control = pd.DataFrame(df_prices[ls_control_ids].mean(1),
-#                             df_prices.index, ['price'])
-#df_tta_lg_control['time'] = df_tta_lg_control.index
-
-ls_rows_ttac= []
-for id_station, ls_ta_comp in dict_ls_ta_comp.items():
-  if (id_station in ls_keep_ids) and\
-     (df_info.ix[id_station]['group'] != 'TOTAL'):
-    # Need to have pp change and dates of transition
-    ls_ta_comp = [(comp_id, distance) for comp_id, distance in ls_ta_comp\
-                                      if comp_id in ls_ta_chge_ids]
-    # todo: refine if several (first date? or closest?)
-    if (ls_ta_comp) and\
-       (ls_ta_comp[0][1] <= 3):
-      id_ta = ls_ta_comp[0][0]
-      distance = ls_ta_comp[0][1]
-      date_beg = df_info_ta.ix[id_ta]['date_beg']
-      date_end = df_info_ta.ix[id_ta]['date_end']
-      df_lg_ttac = pd.DataFrame(df_prices[id_station].values,
-                                df_prices.index, ['price'])
-      df_lg_ttac.loc[date_beg:date_end, 'price'] = np.nan
-      df_lg_ttac['time'] = df_lg_ttac.index
-      df_lg_ttac['time'] = df_lg_ttac['time'].apply(lambda x: x.strftime('%Y-%m-%d'))
-      df_lg_ttac['treatment'] = 0
-      df_lg_ttac.loc[date_end:, 'treatment'] = 1
-      #df_lg_ttac['id_station'] = id_station
-      #df_lg_ttac['group'] = df_info.ix[id_station]['group']
-      #df_lg_ttac['group_type'] = df_info.ix[id_station]['group_type']
-      #df_lg_ttac['reg'] = df_info.ix[id_station]['reg']
-      #df_lg_ttac['distance'] = distance
-      
-      # Control group
-      dpt = df_info.ix[id_station]['dpt']
-      ls_dpt_control_ids = [id_ctrl for id_ctrl in df_info.index[df_info['dpt'] == dpt]\
-                              if id_ctrl in ls_control_ids]
-      if (ls_dpt_control_ids) and (len(ls_dpt_control_ids) >= 5):
-        se_ctrl = df_prices[ls_dpt_control_ids].mean(1)
-      else:
-        reg = df_info.ix[id_station]['reg']
-        ls_control_ids = [id_ctrl for id_ctrl in df_info.index[df_info['reg'] == reg]\
-                            if id_ctrl in ls_control_ids]
-        se_ctrl = df_prices[ls_control_ids].mean(1)
-      df_lg_ctrl = pd.DataFrame(se_ctrl.values,
-                                df_prices.index, ['price'])
-      df_lg_ctrl['time'] = df_lg_ctrl.index
-      df_lg_ctrl['time'] = df_lg_ctrl['time'].apply(lambda x: x.strftime('%Y-%m-%d'))
-      df_lg_ctrl['treatment'] = 0
-
-      df_lg = pd.concat([df_lg_ttac, df_lg_ctrl], ignore_index = True)
-      
-      reg = smf.ols('price ~ C(time) + treatment',
-                    df_lg).fit()
-      ls_tup_coeffs = zip(reg.params.index.values.tolist(),
-                          reg.params.values.tolist(),
-                          reg.bse.values.tolist(),
-                          reg.tvalues.values.tolist(),
-                          reg.pvalues.values.tolist())
-      df_tc = pd.DataFrame(ls_tup_coeffs, columns = ['name', 'coeff', 'se', 'tval', 'pval'])
-      ls_rows_ttac.append((id_station,
-                           id_ta,
-                           distance,
-                           df_info.ix[id_station]['group'],
-                           df_info.ix[id_station]['group_type'],
-                           df_tc[df_tc['name'] == 'treatment']['coeff'].values[0],
-                           df_tc[df_tc['name'] == 'treatment']['se'].values[0],
-                           df_tc[df_tc['name'] == 'treatment']['tval'].values[0]))
-
-df_coeffs_ttac = pd.DataFrame(ls_rows_ttac, columns = ['id_station',
-                                                       'id_ta',
-                                                       'distance',
-                                                       'group',
-                                                       'group_type',
-                                                       'coeff',
-                                                       'se',
-                                                       'tval'])
+## ##########################################
+## LONG PANEL: COMP OF TOTAL => TOTAL ACCESS
+## ##########################################
+#
+#ls_ta_chge_ids = list(df_info_ta.index[(df_info_ta['pp_chge'] >= 0.05) &\
+#                                       (~pd.isnull(df_info_ta['date_beg']))])
+#
+##df_tta_lg_control = pd.DataFrame(df_prices[ls_control_ids].mean(1),
+##                             df_prices.index, ['price'])
+##df_tta_lg_control['time'] = df_tta_lg_control.index
+#
+#ls_rows_ttac= []
+#for id_station, ls_ta_comp in dict_ls_ta_comp.items():
+#  if (id_station in ls_keep_ids) and\
+#     (df_info.ix[id_station]['group'] != 'TOTAL'):
+#    # Need to have pp change and dates of transition
+#    ls_ta_comp = [(comp_id, distance) for comp_id, distance in ls_ta_comp\
+#                                      if comp_id in ls_ta_chge_ids]
+#    # todo: refine if several (first date? or closest?)
+#    if (ls_ta_comp) and\
+#       (ls_ta_comp[0][1] <= 3):
+#      id_ta = ls_ta_comp[0][0]
+#      distance = ls_ta_comp[0][1]
+#      date_beg = df_info_ta.ix[id_ta]['date_beg']
+#      date_end = df_info_ta.ix[id_ta]['date_end']
+#      df_lg_ttac = pd.DataFrame(df_prices[id_station].values,
+#                                df_prices.index, ['price'])
+#      df_lg_ttac.loc[date_beg:date_end, 'price'] = np.nan
+#      df_lg_ttac['time'] = df_lg_ttac.index
+#      df_lg_ttac['time'] = df_lg_ttac['time'].apply(lambda x: x.strftime('%Y-%m-%d'))
+#      df_lg_ttac['treatment'] = 0
+#      df_lg_ttac.loc[date_end:, 'treatment'] = 1
+#      #df_lg_ttac['id_station'] = id_station
+#      #df_lg_ttac['group'] = df_info.ix[id_station]['group']
+#      #df_lg_ttac['group_type'] = df_info.ix[id_station]['group_type']
+#      #df_lg_ttac['reg'] = df_info.ix[id_station]['reg']
+#      #df_lg_ttac['distance'] = distance
+#      
+#      # Control group
+#      dpt = df_info.ix[id_station]['dpt']
+#      ls_dpt_control_ids = [id_ctrl for id_ctrl in df_info.index[df_info['dpt'] == dpt]\
+#                              if id_ctrl in ls_control_ids]
+#      if (ls_dpt_control_ids) and (len(ls_dpt_control_ids) >= 5):
+#        se_ctrl = df_prices[ls_dpt_control_ids].mean(1)
+#      else:
+#        reg = df_info.ix[id_station]['reg']
+#        ls_reg_control_ids = [id_ctrl for id_ctrl in df_info.index[df_info['reg'] == reg]\
+#                                if id_ctrl in ls_control_ids]
+#        se_ctrl = df_prices[ls_reg_control_ids].mean(1)
+#      df_lg_ctrl = pd.DataFrame(se_ctrl.values,
+#                                df_prices.index, ['price'])
+#      df_lg_ctrl['time'] = df_lg_ctrl.index
+#      df_lg_ctrl['time'] = df_lg_ctrl['time'].apply(lambda x: x.strftime('%Y-%m-%d'))
+#      df_lg_ctrl['treatment'] = 0
+#
+#      df_lg = pd.concat([df_lg_ttac, df_lg_ctrl], ignore_index = True)
+#      
+#      reg = smf.ols('price ~ C(time) + treatment',
+#                    df_lg).fit()
+#      ls_tup_coeffs = zip(reg.params.index.values.tolist(),
+#                          reg.params.values.tolist(),
+#                          reg.bse.values.tolist(),
+#                          reg.tvalues.values.tolist(),
+#                          reg.pvalues.values.tolist())
+#      df_tc = pd.DataFrame(ls_tup_coeffs, columns = ['name', 'coeff', 'se', 'tval', 'pval'])
+#
+#      ls_rows_ttac.append((id_station,
+#                           id_ta,
+#                           date_end.strftime('%Y/%m/%d'),
+#                           distance,
+#                           df_info.ix[id_station]['group'],
+#                           df_info.ix[id_station]['group_type'],
+#                           df_tc[df_tc['name'] == 'treatment']['coeff'].values[0],
+#                           df_tc[df_tc['name'] == 'treatment']['se'].values[0],
+#                           df_tc[df_tc['name'] == 'treatment']['tval'].values[0]))
+#
+#df_coeffs_ttac = pd.DataFrame(ls_rows_ttac, columns = ['id_station',
+#                                                       'id_ta',
+#                                                       'date_chge',
+#                                                       'distance',
+#                                                       'group',
+#                                                       'group_type',
+#                                                       'coeff',
+#                                                       'se',
+#                                                       'tval'])
 
 #df_lg_ttac = pd.concat(ls_df_lg_ttac, ignore_index = True)
 #df_lg_ttac['time'] = df_lg_ttac['time'].apply(lambda x: x.strftime('%Y-%m-%d'))
@@ -219,17 +227,17 @@ for id_station, ls_comp in dict_ls_comp.items():
     if ls_elf_comp:
       id_temp = ls_elf_comp[0][0]
       distance = ls_elf_comp[0][1]
-      date_chge = df_info_ta.ix[id_ta]['day_1']
+      date_chge = df_info_ta.ix[id_temp]['day_1']
       date_beg_nan = df_info_ta.ix[id_temp]['day_1'] - pd.Timedelta(days = 10)
       date_end_nan = df_info_ta.ix[id_temp]['day_1'] + pd.Timedelta(days = 10)
       df_lg_elfc = pd.DataFrame(df_prices[id_station].values,
                                 df_prices.index, ['price'])
       df_lg_elfc.loc[date_beg_nan:date_end_nan, 'price'] = np.nan
       df_lg_elfc['time'] = df_lg_elfc.index
-      # df_lg_elfc['time'] = df_lg_elfc['time'].apply(lambda x: x.strftime('%Y-%m-%d'))
-      df_lg_elfc['id_station'] = id_station
+      df_lg_elfc['time'] = df_lg_elfc['time'].apply(lambda x: x.strftime('%Y-%m-%d'))
       df_lg_elfc['treatment'] = 0
       df_lg_elfc.loc[date_end_nan:, 'treatment'] = 1
+      #df_lg_elfc['id_station'] = id_station
       #df_lg_elfc['group'] = df_info.ix[id_station]['group']
       #df_lg_elfc['group_type'] = df_info.ix[id_station]['group_type']
       #df_lg_elfc['reg'] = df_info.ix[id_station]['reg']
@@ -243,16 +251,16 @@ for id_station, ls_comp in dict_ls_comp.items():
         se_ctrl = df_prices[ls_dpt_control_ids].mean(1)
       else:
         reg = df_info.ix[id_station]['reg']
-        ls_control_ids = [id_ctrl for id_ctrl in df_info.index[df_info['reg'] == reg]\
-                            if id_ctrl in ls_control_ids]
-        se_ctrl = df_prices[ls_control_ids].mean(1)
+        ls_reg_control_ids = [id_ctrl for id_ctrl in df_info.index[df_info['reg'] == reg]\
+                                if id_ctrl in ls_control_ids]
+        se_ctrl = df_prices[ls_reg_control_ids].mean(1)
       df_lg_ctrl = pd.DataFrame(se_ctrl.values,
                                 df_prices.index, ['price'])
       df_lg_ctrl['time'] = df_lg_ctrl.index
       df_lg_ctrl['time'] = df_lg_ctrl['time'].apply(lambda x: x.strftime('%Y-%m-%d'))
       df_lg_ctrl['treatment'] = 0
 
-      df_lg = pd.concat([df_lg_ttac, df_lg_ctrl], ignore_index = True)
+      df_lg = pd.concat([df_lg_elfc, df_lg_ctrl], ignore_index = True)
       
       reg = smf.ols('price ~ C(time) + treatment',
                     df_lg).fit()
@@ -263,7 +271,8 @@ for id_station, ls_comp in dict_ls_comp.items():
                           reg.pvalues.values.tolist())
       df_tc = pd.DataFrame(ls_tup_coeffs, columns = ['name', 'coeff', 'se', 'tval', 'pval'])
       ls_rows_elfc.append((id_station,
-                           id_ta,
+                           id_temp,
+                           date_chge.strftime('%Y/%m/%d'),
                            distance,
                            df_info.ix[id_station]['group'],
                            df_info.ix[id_station]['group_type'],
@@ -272,7 +281,8 @@ for id_station, ls_comp in dict_ls_comp.items():
                            df_tc[df_tc['name'] == 'treatment']['tval'].values[0]))
 
 df_coeffs_elfc = pd.DataFrame(ls_rows_elfc, columns = ['id_station',
-                                                       'id_ta',
+                                                       'id_elf',
+                                                       'date_chge',
                                                        'distance',
                                                        'group',
                                                        'group_type',
