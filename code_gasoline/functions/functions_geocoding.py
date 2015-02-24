aa1c5835f8e60136c2be6dba84e01703dd2333c6
etiennecha <echamayou@gmail.com>
2015-02-24 18:24:56 +0100

    update geo

--- code_gasoline/functions/functions_geocoding.py
+++ code_gasoline/functions/functions_geocoding.py
@@ -2,38 +2,82 @@
 # -*- coding: utf-8 -*-
 
 import os, sys
-import re
 import json
 import urllib2
 import time
 
-def dec_json(chemin):
-  with open(chemin, 'r') as fichier:
-    return json.loads(fichier.read())
+def dec_json(path_file):
+  with open(path_file, 'r') as f:
+    return json.loads(f.read())
 
-def enc_stock_json(database, chemin):
-  with open(chemin, 'w') as fichier:
-    json.dump(database, fichier)
+def enc_json(json_obj, path_file):
+  with open(path_file, 'w') as f:
+    json.dump(json_obj, f)
 
-def geocode_via_mapquest(location):
-  # systematic queries forbidden...
-  location = urllib2.quote(location.encode('utf-8'))
-  url = u'http://open.mapquestapi.com/nominatim/v1/search?q=%s&' %location +\
-        u'format=json&polygon=1&addressdetails=1&countrycodes=fr&viewbox=-5.133333,51.0833,9.55,41.333333'
+def get_google_direction(key_api, origin, destination, region = 'FR'):
+  origin = urllib2.quote(origin.encode('utf-8'))
+  destination = urllib2.quote(destination.encode('utf-8'))
+  url = 'https://maps.googleapis.com/maps/api/directions/json?'+\
+      'origin={:s}&destination={:s}&region={:s}&key={:s}'.format(origin,
+                                                                 destination,
+                                                                 region,
+                                                                 key_api)
   geocoding_response = urllib2.urlopen(url)
   json_response = json.loads(geocoding_response.read())
   return json_response
 
-def geocode_via_google(key_api, location):
-  location += ', France'
+def geocode_via_google(key_api, location, region = 'FR'):
   location = urllib2.quote(location.encode('utf-8'))
   url = u'https://maps.googleapis.com/maps/api/geocode/json?' +\
-        u'address=%s&region=FR&sensor=false&key=%s' %(location, key_api)
+        u'address={:s}&region={:s}&sensor=false&key={:s}'.format(location,
+                                                                 region,
+                                                                 key_api)
   geocoding_response = urllib2.urlopen(url)
   json_response = json.loads(geocoding_response.read())
   return json_response
 
-def get_best_geocoding_info(geocoding_info):
+def geocode_via_google_textsearch(key_api, query, all_pages = False):
+  """
+  @query: text string to search
+
+  Tight quota (100 daily) hence best practice: 
+  - first result page only (if helps?) 
+  - restrict type
+  """
+  results = []
+  query = urllib2.quote(query.encode('utf-8'))
+  url = u'https://maps.googleapis.com/maps/api/place/textsearch/json?' +\
+        u'query={:s}&key={:s}'.format(query,
+                                      key_api)
+  geocoding_response = urllib2.urlopen(url)
+  json_response = json.loads(geocoding_response.read())
+  status = json_response['status']
+  results = json_response['results']
+  if all_pages:
+    while 'next_page_token' in json_response.keys():
+      time.sleep(2)
+      url = u'https://maps.googleapis.com/maps/api/place/textsearch/json?' +\
+            u'pagetoken={:s}&key={:s}'.format(json_response['next_page_token'],
+                                              key_api)
+      geocoding_response = urllib2.urlopen(url)
+      json_response = json.loads(geocoding_response.read())
+      status = json_response['status']
+      results += json_response['results']
+  return status, results
+
+def geocode_via_mapquest(location):
+  """
+  Check guidelines: systematic queries forbidden?
+  """
+  location = urllib2.quote(location.encode('utf-8'))
+  url = u'http://open.mapquestapi.com/nominatim/v1/search?q={:s}&'.format(location) +\
+        u'format=json&polygon=1&addressdetails=1&'+\
+        u'countrycodes=fr&viewbox=-5.133333,51.0833,9.55,41.333333'
+  geocoding_response = urllib2.urlopen(url)
+  json_response = json.loads(geocoding_response.read())
+  return json_response
+
+def get_best_google_geocoding_info(geocoding_info):
   """
   Find best quality geocoding result in a list of standard Google geocoding results
   Supports lists of None (geocoding not yet done)
@@ -52,72 +96,72 @@ def get_best_geocoding_info(geocoding_info):
       score = dict_score_stats[info['results'][0]['geometry']['location_type']]
   return best_geocoding_info
 
-def get_stats_geocoding(master_geocoding):
+def get_google_geocoding_stats(dict_geocoding):
   """ 
   Provide descriptive stats on Google geocoding results
   Supports no address and geocoding not yet done
+  #ls_location_type = ['ROOFTOP',
+  #                    'RANGE_INTERPOLATED',
+  #                    'GEOMETRIC_CENTER',
+  #                    'APPROXIMATE']
+  #ls_bad_status = ['INVALID_REQUEST',
+  #                 'ZERO_RESULTS']
   """
-  score_stats =  {'ROOFTOP' : [],
-                  'RANGE_INTERPOLATED' : [],
-                  'GEOMETRIC_CENTER' : [],
-                  'APPROXIMATE' : [],
-                  'ZERO_RESULTS' : [],
-                  'INVALID_REQUEST' : []}
-  ls_geocoded_but_weird = []
-  ls_not_geocoded = []
-  for indiv_id, geo_info in master_geocoding.iteritems():
-    if geo_info[1]:
-      best_geocoding_info = get_best_geocoding_info(geo_info[1])
-      if best_geocoding_info:
-        score_stats[best_geocoding_info['results'][0]['geometry']['location_type']].append(indiv_id)
-      elif 'INVALID_REQUEST' in [dict_info['status'] for dict_info in geo_info[1] if dict_info]:
-        score_stats['INVALID_REQUEST'].append(indiv_id)
-      elif 'ZERO_RESULTS' in [dict_info['status'] for dict_info in geo_info[1] if dict_info]:
-        score_stats['ZERO_RESULTS'].append(indiv_id)
-      else:
-        ls_geocoded_but_weird.append(indiv_id)
+  dict_score_stats = {'TO_DO' : []}
+  for indiv_id, indiv_geo in dict_geocoding.items():
+    best_indiv_geo = get_best_google_geocoding_info(indiv_geo[1])
+    if best_indiv_geo:
+      dict_score_stats.setdefault(\
+        best_indiv_geo['results'][0]['geometry']['location_type'], []).append(indiv_id)
     else:
-      ls_not_geocoded.append(indiv_id)
-  return score_stats, ls_geocoded_but_weird, ls_not_geocoded
-
-# path_data: data folder at different locations at CREST vs. HOME
-if os.path.exists(r'W:\Bureau\Etienne_work\Data'):
-  path_data = r'W:\Bureau\Etienne_work\Data'
-else:
-  path_data = r'C:\Users\etna\Desktop\Etienne_work\Data'
-geocoding_folder = r'\data_gasoline\data_source\geolocalisation\geocoding'
+      if [x for x in indiv_geo[1] if x]:
+          dict_score_stats.setdefault(\
+            indiv_geo[1][0]['status'], []).append(indiv_id)
+      else:
+        dict_score_stats['TO_DO'].append(indiv_id)
+  return dict_score_stats
 
 if __name__=="__main__":
   
-  test_google =  geocode_via_google(u'r.n 7,84430 mondragon')
-  test_mapquest = geocode_via_mapquest(u'9 rue des Grandes Allées, 02600 Villers-Cotterêts')
+  # Need to have a google api key
+  path_data_0 = u'W:\Bureau\Etienne_work\Data'
+  path_data_1 = u'C:\Bureau\Etienne_work\Data'
+  if not os.path.exists(path_data_0):
+    path_data_0 = path_data_1
+  path_dir_api_keys = os.path.join(path_data_0, 'api_keys')
+  with open(os.path.join(path_dir_api_keys, 'key_google_api.txt'), 'r') as f:
+    key_google_api = f.read()
   
-  # TODO: update following
+  # Geocoding tests: Google vs. Mapquest
+  location = u'9 rue des Grandes Allées, 02600 Villers-Cotterêts'
+  test_google_geocoding =  geocode_via_google(key_google_api,
+                                              location)
+  test_mapquest_geocoding = geocode_via_mapquest(location)
   
-  # dict_ids_bad_gps = dec_json(path_data + geocoding_folder + r'\current_master_geocoding')
-  # for id, info in dict_ids_bad_gps.iteritems():
-    # if 'gps_geocoding' not in info or info['gps_geocoding']['status'] == u'OVER_QUERY_LIMIT':
-      # try:
-        # address = ', '.join(info['address'])
-        # info['gps_geocoding'] = geocode_via_google(address.encode('utf-8'))
-        # time.sleep(0.15)
-      # except Exception, e:
-        # print id, e
-        # info['gps_geocoding'] = None
-  # # enc_stock_json(dict_ids_bad_gps, machine_path + r'\\' + geocoding_folder + r'\\current_master_geocoding')
-  
-  # http://open.mapquestapi.com/nominatim/v1/search?q=14%20rue%20martignon,%20rueil%20malmaison&format=json&polygon=1&addressdetails=1&countrycodes=fr&viewbox=-5.133333,51.0833,9.55,41.333333
-  
-  # France box incl. Corsica
-  # 51.0833, -5.133333 North West
-  # 41.333333, 9.55 South East
-  
-  # Google bounds example : bounds=34.172684,-118.604794|34.236144,-118.500938
-  # France: bounds=41.333333,-5.133333|51.0833,9.55
-  # addr_1 = 14%20%rue%20martignon,%2092500%20rueil%20malmaison
-  # addr_2 = r.n%207,84430%20mondragon
-  # http://maps.googleapis.com/maps/api/geocode/json?address=r.n%207,84430%20mondragon&region=FR&sensor=false&bounds=41.333333,-5.133333|51.0833,9.55
-  
-  # Mapquest viewbox: viewbox=left,top,right,bottom
-  # France: -5.133333,51.0833,9.55,41.333333 
-  
+  # Routing test: Google directions with addresses and gps
+  origin = u'7 rue Marie Bonaparte, Saint Cloud'
+  destination = u'15 Boulevard Gabriel Péri, Malakoff'
+  test_google_direction_adr = get_google_direction(key_google_api,
+                                                   origin,
+                                                   destination)
+
+  gps_origin = '48.853 2.216'
+  gps_destination = '48.819 2.300'
+  test_google_direction_gps = get_google_direction(key_google_api,
+                                                   gps_origin,
+                                                   gps_destination)
+
+  ## Google Text search (expensive in quota)
+  #query = 'Franprix, Paris, France'
+  #test_google_textsearch = geocode_via_google_textsearch(key_google_api,
+  #                                                       query,
+  #                                                       all_pages = True)
+  #import pandas as pd 
+  #ls_rows_ts = [[x['name'],
+  #               x['formatted_address'],
+  #               '-'.join(x['types'])] for x in test_google_text_search[1]]
+  #df_ts = pd.DataFrame(ls_rows_ts)
+
+  #France box incl. Corsica
+  #51.0833, -5.133333 North West
+  #41.333333, 9.55 South East
--- code_qlmc/functions/functions_maps.py
+++ code_qlmc/functions/functions_maps.py
@@ -1,12 +1,17 @@
 ﻿#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from mpl_toolkits.basemap import Basemap
+import math
+import urllib2
+import StringIO
+from PIL import Image
 
-# Convenience functions for working with colour ramps and bars
 def colorbar_index(ncolors, cmap, labels=None, **kwargs):
   """
   This is a convenience function to stop you making off-by-one errors
@@ -46,3 +51,55 @@ def cmap_discretize(cmap, N):
   for ki, key in enumerate(('red', 'green', 'blue')):
     cdict[key] = [(indices[i], colors_rgba[i - 1, ki], colors_rgba[i, ki]) for i in xrange(N + 1)]
   return matplotlib.colors.LinearSegmentedColormap(cmap.name + "_%d" % N, cdict, 1024)
+
+def deg2num(lat_deg, lon_deg, zoom):
+  """
+  Tiles to request (OSM) based on gps coordinates and zoom
+  """
+  lat_rad = math.radians(lat_deg)
+  n = 2.0 ** zoom
+  xtile = int((lon_deg + 180.0) / 360.0 * n)
+  ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
+  return (xtile, ytile)
+
+def num2deg(xtile, ytile, zoom):
+  """
+  This returns the NW-corner of the square. 
+  Use the function with xtile+1 and/or ytile+1 to get the other corners. 
+  With xtile+0.5 & ytile+0.5 it will return the center of the tile.
+  """
+  n = 2.0 ** zoom
+  lon_deg = xtile / n * 360.0 - 180.0
+  lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+  lat_deg = math.degrees(lat_rad)
+  return (lat_deg, lon_deg)
+
+def getImageCluster(lat_deg, lon_deg, delta_lat,  delta_long, zoom):
+    #base_url = r"http://otile1.mqcdn.com/tiles/1.0.0/map/"
+    base_url= r"http://a.tile.openstreetmap.org/"
+    smurl = base_url + r"{0}/{1}/{2}.png"
+    xmin, ymax = deg2num(lat_deg, lon_deg, zoom)
+    xmax, ymin = deg2num(lat_deg + delta_lat, lon_deg + delta_long, zoom)
+
+    bbox_ul = num2deg(xmin, ymin, zoom)
+    bbox_ll = num2deg(xmin, ymax + 1, zoom)
+    #print bbox_ul, bbox_ll
+
+    bbox_ur = num2deg(xmax + 1, ymin, zoom)
+    bbox_lr = num2deg(xmax + 1, ymax +1, zoom)
+    #print bbox_ur, bbox_lr
+
+    Cluster = Image.new('RGB',((xmax-xmin+1)*256-1,(ymax-ymin+1)*256-1) )
+    for xtile in range(xmin, xmax+1):
+        for ytile in range(ymin,  ymax+1):
+            try:
+                imgurl=smurl.format(zoom, xtile, ytile)
+                print("Opening: " + imgurl)
+                imgstr = urllib2.urlopen(imgurl).read()
+                tile = Image.open(StringIO.StringIO(imgstr))
+                Cluster.paste(tile, box=((xtile-xmin)*255 ,  (ytile-ymin)*255))
+            except: 
+                print("Couldn't download image")
+                tile = None
+
+    return Cluster, [bbox_ll[1], bbox_ll[0], bbox_ur[1], bbox_ur[0]]
