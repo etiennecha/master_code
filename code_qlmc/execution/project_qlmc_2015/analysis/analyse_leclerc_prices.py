22187b4769449b42e88c3e6432c801201f6b8e54
etiennecha <echamayou@gmail.com>
2015-03-24 17:40:35 +0100

    update qlmc 2015 analysis

---
+++ code_qlmc/execution/project_qlmc_2015/analysis/analyse_leclerc_prices.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import add_to_path
+from add_to_path import *
+import os, sys
+import httplib
+import urllib, urllib2
+from bs4 import BeautifulSoup
+import re
+import json
+import pandas as pd
+from functions_generic_qlmc import *
+import numpy as np
+import matplotlib.pyplot as plt
+import textwrap
+
+pd.set_option('float_format', '{:,.2f}'.format)
+format_float_int = lambda x: '{:10,.0f}'.format(x)
+format_float_float = lambda x: '{:10,.2f}'.format(x)
+
+path_qlmc_scraped = os.path.join(path_data,
+                                  'data_qlmc',
+                                  'data_source',
+                                  'data_scraped')
+
+path_csv = os.path.join(path_data,
+                        'data_qlmc',
+                        'data_built',
+                        'data_csv')
+
+df_stores = pd.read_csv(os.path.join(path_csv,
+                                     'qlmc_scraped',
+                                     'df_stores.csv'),
+                        encoding = 'utf-8')
+
+df_comp = pd.read_csv(os.path.join(path_csv,
+                                   'qlmc_scraped',
+                                   'df_competitors.csv'),
+                      encoding = 'utf-8')
+
+df_france = pd.read_csv(os.path.join(path_csv,
+                                     'qlmc_scraped',
+                                     'df_france.csv'),
+                        encoding = 'utf-8')
+
+# Restrict to leclerc (could drop rest?)
+df_leclerc = df_france[df_france['chain'] == 'LEC'].copy()
+
+# Count stores present in data
+print u'\nNb Leclerc: {:d}'.format(len(df_leclerc['store_id'].unique()))
+
+# Count products present in data
+# (todo: never same product under two families/subfamilies?)
+print u'\nNb products: {:d}'.format(len(df_leclerc['product'].unique()))
+
+# Nb of products by leclerc
+se_nb_prod = df_leclerc[['store_id', 'product']].groupby('store_id').agg([len])['product']
+print u'\nDescribe nb prod by store:'
+print se_nb_prod.describe()
+
+# Most common products
+print u'\nShow most common products:'
+se_prod_vc = df_leclerc['product'].value_counts()
+print se_prod_vc[0:30].to_string()
+
+# Price distribution of most common products
+ls_df_desc = []
+for product in se_prod_vc.index[0:30]:
+  if len(df_leclerc[df_leclerc['product'] == product]['store_id'].unique()) !=\
+      se_prod_vc[product]:
+    print 'Pbm: product {:s} listed twice for one store'.format(product)
+  ls_df_desc.append(df_leclerc[df_leclerc['product'] == product]['price'].describe())
+df_desc = pd.concat(ls_df_desc, keys = se_prod_vc.index[0:30], axis = 1)
+df_desc = df_desc.T
+
+df_desc['cv'] = df_desc['std'] / df_desc['mean']
+df_desc['gfs'] = df_desc['mean'] - df_desc['min']
+df_desc['range'] = df_desc['max'] - df_desc['min']
+df_desc['range_2'] = df_desc['75%'] - df_desc['25%']
+
+print df_desc[['mean', 'std', 'cv', 'gfs', 'range', 'range_2']].to_string()
+
+df_prod_prices = df_leclerc[df_leclerc['product'].isin(se_prod_vc[0:1].index)]\
+                   [['product', 'price', 'store_id']].copy()
+df_prod_prices = df_prod_prices.pivot(index='store_id', columns='product', values='price')
+# improve boxplot display
+# http://matplotlib.org/examples/pylab_examples/boxplot_demo2.html
+# todo: update matplotlib http://stackoverflow.com/questions/21997897/ (ctd)
+# changing-what-the-ends-of-whiskers-represent-in-matplotlibs-boxplot-function
+ax = df_prod_prices.plot(kind = 'box', whis = [0.10, 0.90])
+# ax.get_xticklabels()[0].get_text()
+# textwrap.fill(ax.get_xticklabels()[0].get_text(), width = 20)
+ax.set_xticklabels([textwrap.fill(x.get_text(), 20) for x in ax.get_xticklabels()])
+plt.show()
+
+# todo: find most common prod within important categories and draw maps
+# todo: restrict to most common prod and run regressions (check robustness?)
+# moustache plot by product?
--- code_qlmc/execution/project_qlmc_2015/formatting/check_pairs_not_scraped.py
+++ code_qlmc/execution/project_qlmc_2015/formatting/check_pairs_not_scraped.py
@@ -39,5 +39,44 @@ dict_reg_leclerc = dec_json(os.path.join(path_qlmc_scraped,
 dict_leclerc_comp = dec_json(os.path.join(path_qlmc_scraped,
                                           'dict_leclerc_comp.json'))
 
-# todo: load df_comp (contains pairs actually collected)
-# todo: compare with dict_leclerc_comp (pairs on website)
+df_comp = pd.read_csv(os.path.join(path_csv,
+                                   'qlmc_scraped',
+                                   'df_competitors.csv'))
+
+# Exclude Leclerc from competitors (not compared on website)
+for leclerc_id, ls_comp in dict_leclerc_comp.items():
+  ls_comp_cl = [comp for comp in ls_comp if comp['signCode'] != u'LEC']
+  dict_leclerc_comp[leclerc_id] = ls_comp_cl
+
+# Count all competitor pairs from website
+ls_all_pairs = [(leclerc_id, comp['slug']\
+                  for leclerc_id, ls_comp in dict_leclerc_comp.items()\
+                    for comp in ls_comp]
+
+# Count competitor pairs actually queried
+# NB: fragile... loop on dict and order matters
+ls_covered_comp = [] # should be stored and updated?
+ls_collect_pairs = []
+dict_reg_pairs = {}
+for region, ls_leclerc in dict_reg_leclerc.items():
+  ls_reg_pairs = []
+  for leclerc in ls_leclerc:
+    ls_comp = dict_leclerc_comp[leclerc['slug']]
+    # keep competitors not met in scrapping so far (store itself included in list)
+    ls_comp_todo = [comp for comp in ls_comp if\
+                      (comp['slug'] not in ls_covered_comp)]
+    # if all scraped, take first one to get current Leclerc's prices
+    if not ls_comp_todo:
+      ls_comp_todo = ls_comp[:1]
+    for comp in ls_comp_todo:
+      ls_collect_pairs.append((leclerc['slug'], comp['slug']))
+      ls_reg_pairs.append((leclerc['slug'], comp['slug']))
+      ls_covered_comp.append(comp['slug'])
+  dict_reg_pairs[region] = ls_reg_pairs
+
+# List competitor pairs not collected (on purpose)
+ls_exclude_pairs = [(leclerc_id, comp_id) for (leclerc_id, comp_id) in ls_all_pairs\
+                      if (leclerc_id, comp_id) not in ls_collect_pairs]
+
+print u'\nTotal nb pairs listed on website {:d}'.format(len(ls_all_pairs))
+print u'\nNumber of pairs queried {:d}'.format(len(ls_pairs))
