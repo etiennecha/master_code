985306040fa07ecb03fcb6f85f2a99f1bacbb162
etiennecha <echamayou@gmail.com>
2015-03-26 17:25:15 +0100

    update qlmc 2015 scraping

--- code_qlmc/execution/project_qlmc_2015/report/ta_overview_products.py
+++ code_qlmc/execution/project_qlmc_2015/report/ta_overview_products.py
@@ -77,38 +77,13 @@ se_chain_vc = df_france['chain'].value_counts()
 #               'REC' : 'RECORD',
 #               'HAU' : "LES HALLES D'AUCHAN"}
 
-# Price distribution of one product at each chain
-# todo: make sure each store caries each product only once in data
-ls_df_desc = []
-ref_product = u'DANONE YAOURT DANONE NATURE 4X125G' # se_prod_vc.index[0]
-df_product = df_france[df_france['product'] == ref_product]
+df_france.set_index('product', inplace = True)
+df_france['freq_prod'] = se_prod_vc
+df_france.reset_index(inplace = True)
 
-for chain in se_chain_vc.index[0:30]:
-  ls_df_desc.append(df_product[df_product['chain'] == chain]['price'].describe())
-df_desc = pd.concat(ls_df_desc, keys = se_chain_vc.index[0:30], axis = 1)
-df_desc = df_desc.T
+df_u_prod = df_france.drop_duplicates('product')
+df_u_prod = df_u_prod[['family', 'subfamily', 'product', 'freq_prod']]
 
-df_desc['cv'] = df_desc['std'] / df_desc['mean']
-df_desc['gfs'] = df_desc['mean'] - df_desc['min']
-df_desc['range'] = df_desc['max'] - df_desc['min']
-df_desc['range_2'] = df_desc['75%'] - df_desc['25%']
-
-#print df_desc[['count', 'mean', 'std', 'cv', 'gfs', 'range', 'range_2']].to_string()
-print df_desc.to_string()
-
-## following filled with nan... must be better way
-#df_prod_prices = df_product.pivot(index='store_id', columns='chain', values='price')
-## improve boxplot display
-## http://matplotlib.org/examples/pylab_examples/boxplot_demo2.html
-## todo: update matplotlib http://stackoverflow.com/questions/21997897/ (ctd)
-## changing-what-the-ends-of-whiskers-represent-in-matplotlibs-boxplot-function
-#ax = df_prod_prices[['ITM', 'USM', 'CAR', 'LEC', 'AUC', 'GEA']]\
-#       .plot(kind = 'box') #, whis = [0.10, 0.90])
-## ax.get_xticklabels()[0].get_text()
-## textwrap.fill(ax.get_xticklabels()[0].get_text(), width = 20)
-#ax.set_xticklabels([textwrap.fill(x.get_text(), 20) for x in ax.get_xticklabels()])
-#plt.show()
-#
-## todo: find most common prod within important categories and draw maps
-## todo: restrict to most common prod and run regressions (check robustness?)
-## moustache plot by product?
+df_u_prod.sort('freq_prod', ascending = False, inplace = True)
+df_u_prod.set_index(['family', 'subfamily', 'product'], inplace = True)
+df_u_prod.sort_index(inplace = True)
