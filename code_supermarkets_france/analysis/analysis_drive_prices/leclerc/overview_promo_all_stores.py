31c8b7452eedaa76d24e8b4c8510a6e78ca86ae7
etna <echamayou@gmail.com>
2015-05-28 08:54:36 +0200

    update leclerc processing

--- code_supermarkets_france/analysis/analysis_drive_prices/leclerc/overview_promo_all_stores.py
+++ code_supermarkets_france/analysis/analysis_drive_prices/leclerc/overview_promo_all_stores.py
@@ -31,28 +31,75 @@ df_master = dict_df['df_master_leclerc_2015']
 df_prices = dict_df['df_prices_leclerc_2015']
 df_products = dict_df['df_products_leclerc_2015']
 
-# RESTRICTION TO ONE DAY
-df_sub_master = df_master[(df_master['date'] == '20150508')]
-df_sub_prices = df_prices[(df_prices['date'] == '20150508')]
+## RESTRICTION TO ONE DAY
+#
+#df_sub_master = df_master[(df_master['date'] == '20150508')]
+#df_sub_prices = df_prices[(df_prices['date'] == '20150508')]
+#
+## PROMO
+#df_promo = pd.pivot_table(df_sub_prices[df_sub_prices['dum_promo'] == True],
+#                          columns = 'store',
+#                          index = ['title', 'label'],
+#                          aggfunc = 'size')
+#df_promo.fillna(0, inplace = True)
+#df_promo['nb_stores'] = df_promo.sum(1)
+#df_promo.sort('nb_stores', ascending = False, inplace = True)
+## could check if not promo because product not available or not offered
+#print df_promo.to_string()
+#
+## LOYALTY
+#df_loyalty = pd.pivot_table(df_sub_prices[~df_sub_prices['loyalty'].isnull()],
+#                            columns = 'store',
+#                            index = ['title', 'label'],
+#                            aggfunc = 'size')
+#df_loyalty.fillna(0, inplace = True)
+#df_loyalty['nb_stores'] = df_loyalty.sum(1)
+#df_loyalty.sort('nb_stores', ascending = False, inplace = True)
+## could check if not loyalty because product not available or not offered
+##print df_loyalty.to_string()
+
+# ALL DAYS: FOCUS ON NB OF STORES FOR EACH PROD
 
 # PROMO
-df_promo = pd.pivot_table(df_sub_prices[df_sub_prices['dum_promo'] == True],
-                          columns = 'store',
-                          index = ['title', 'label'],
-                          aggfunc = 'size')
+
+df_promo = pd.pivot_table(df_prices[df_prices['dum_promo'] == True],
+                            columns = 'store',
+                            index = ['date', 'title', 'label'],
+                            aggfunc = 'size')
 df_promo.fillna(0, inplace = True)
-df_promo['nb_stores'] = df_promo.sum(1)
-df_promo.sort('nb_stores', ascending = False, inplace = True)
-# could check if not promo because product not available or not offered
-print df_promo.to_string()
+df_promo['nb_stores'] = df_promo[df_prices['store'].unique().tolist()].sum(1)
+df_promo.reset_index(drop = False, inplace = True)
+
+print u'\nTop promo (by nb of stores) on first day:'
+df_promo.sort(['date', 'nb_stores'], ascending = False, inplace = True)
+print df_promo[df_promo['date'] == '20150507'][0:10].to_string()
+
+print u'\nPromo by nb of stores over time:'
+df_promo_su = pd.pivot_table(df_promo,
+                               columns = 'nb_stores',
+                               index = 'date',
+                               aggfunc = 'size')
+df_promo_su.fillna(0, inplace = True)
+print df_promo_su.to_string()
 
 # LOYALTY
-df_loyalty = pd.pivot_table(df_sub_prices[~df_sub_prices['loyalty'].isnull()],
+
+df_loyalty = pd.pivot_table(df_prices[~df_prices['loyalty'].isnull()],
                           columns = 'store',
-                          index = ['title', 'label'],
+                          index = ['date', 'title', 'label'],
                           aggfunc = 'size')
 df_loyalty.fillna(0, inplace = True)
-df_loyalty['nb_stores'] = df_loyalty.sum(1)
-df_loyalty.sort('nb_stores', ascending = False, inplace = True)
-# could check if not loyalty because product not available or not offered
-#print df_loyalty.to_string()
+df_loyalty['nb_stores'] = df_loyalty[df_prices['store'].unique().tolist()].sum(1)
+df_loyalty.reset_index(drop = False, inplace = True)
+
+print u'\nTop loyalty promo (by nb of stores) on first day:'
+df_loyalty.sort(['date', 'nb_stores'], ascending = False, inplace = True)
+print df_loyalty[df_loyalty['date'] == '20150507'][0:10].to_string()
+
+print u'\nLoyalty promo by nb of stores over time:'
+df_loyalty_su = pd.pivot_table(df_loyalty,
+                               columns = 'nb_stores',
+                               index = 'date',
+                               aggfunc = 'size')
+df_loyalty_su.fillna(0, inplace = True)
+print df_loyalty_su.to_string()
