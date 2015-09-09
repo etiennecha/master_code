fd2ae941109f03f60d179a73b5e070db65f3505d
etna <echamayou@gmail.com>
2015-09-09 08:59:39 +0200

    update gas comp

--- code_gasoline_france/execution_dispersion/analysis_db/dispersion/pair_likely_competitors.py
+++ code_gasoline_france/execution_dispersion/analysis_db/dispersion/pair_likely_competitors.py
@@ -84,12 +84,18 @@ def extract_id(df_station, id_station):
   id_same.remove(id_station)
   return id_same[0]
 
+df_pairs['abs_mean_spread'] = df_pairs['mean_spread'].abs()
+
 df_pairs.sort('distance', inplace = True)
 
 ls_rows = []
 ls_ids = list(set(df_pairs['id_1'].tolist() + df_pairs['id_2'].tolist()))
+ls_fields = [('pct_same', False),
+             ('abs_mean_spread', True),
+             ('pct_rr', False)]
+
 # seems slow... optimize?
-for id_station in ls_ids[0:1000]:
+for id_station in ls_ids:
   df_station = df_pairs[(df_pairs['id_1'] == id_station) |\
                         (df_pairs['id_2'] == id_station)].copy()
   # closest same group station
@@ -99,24 +105,34 @@ for id_station in ls_ids[0:1000]:
     id_same = extract_id(df_closest_same, id_station)
     distance_same = df_closest_same['distance'].iloc[0]
   # closest competitor station
-  df_closest_comp = df_station[df_station['group_last_1'] != df_station['group_last_2']]
+  df_closest_comp = df_station[df_station['group_last_1'] != df_station['group_last_2']].copy()
   id_comp, distance_comp = None, np.nan
   if len(df_closest_comp) > 0:
     id_comp = extract_id(df_closest_comp, id_station)
     distance_comp = df_closest_comp['distance'].iloc[0]
-  id_pct_same, dist_pct_same, pct_same = None, np.nan, np.nan
-  if len(df_station) > 0:
-    df_station.sort('pct_same', ascending = False, inplace = True)
-    id_pct_same = extract_id(df_station, id_station)
-    dist_pct_same = df_station['distance'].iloc[0]
-    pct_same = df_station['pct_same'].iloc[0]
-  ls_rows.append((id_station, id_same, distance_same, id_comp, distance_comp,
-                  id_pct_same, dist_pct_same, pct_same))
+  row = [id_station, id_same, distance_same, id_comp, distance_comp]
+  df_temp = df_closest_comp # choose all (df_station) or competitors only (df_closest_comp)
+  if len(df_temp) > 0:
+    for field, sort_order in ls_fields:
+      id_field, dist_field, stat_field = None, np.nan, np.nan
+      df_temp.sort(field, ascending = sort_order, inplace = True)
+      id_field = extract_id(df_temp, id_station)
+      dist_field = df_temp['distance'].iloc[0]
+      stat_field = df_temp[field].iloc[0]
+      row += [id_field, dist_field, stat_field]
+  else:
+    for field, sort_order in ls_fields:
+      row += [None, np.nan, np.nan]
+  ls_rows.append(row)
 
 ls_cols = ['id_station',
            'id_friend', 'dist_friend',
-           'id_comp', 'dist_comp',
-           'id_pct_same', 'dist_pct_same', 'pct_same']
+           'id_comp', 'dist_comp'] +\
+          [x for ls_x in [['id_{:s}'.format(field),
+                           'dist_{:s}'.format(field),
+                           field] for field, sort_order in ls_fields] for x in ls_x]
 
 df_lcomp = pd.DataFrame(ls_rows, columns = ls_cols)
 df_lcomp.set_index('id_station', inplace = True)
+
+print df_lcomp.describe().to_string()
