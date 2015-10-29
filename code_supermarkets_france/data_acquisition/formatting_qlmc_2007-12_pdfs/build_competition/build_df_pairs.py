5431da77c90e6e36cca2cd45f410882273c4c0c2
etiennecha <echamayou@gmail.com>
2015-10-29 15:26:37 +0100

    update supermarkets

--- code_supermarkets_france/data_acquisition/formatting_qlmc_2007-12_pdfs/build_competition/build_df_pairs.py
+++ code_supermarkets_france/data_acquisition/formatting_qlmc_2007-12_pdfs/build_competition/build_df_pairs.py
@@ -36,24 +36,28 @@ df_stores = pd.read_csv(os.path.join(path_built_csv,
 
 df_lsa = pd.read_csv(os.path.join(path_built_csv_lsa,
                                   'df_lsa.csv'),
-                     dtype = {u'C_INSEE' : str,
-                              u'C_INSEE_Ardt' : str,
-                              u'C_Postal' : str,
-                              u'SIREN' : str,
-                              u'NIC' : str,
-                              u'SIRET' : str},
-                     parse_dates = [u'Date_Ouv', u'Date_Fer', u'Date_Reouv',
-                                    u'Date_Chg_Enseigne', u'Date_Chg_Surface'],
+                     dtype = {u'c_insee' : str,
+                              u'c_insee_ardt' : str,
+                              u'c_postal' : str,
+                              u'c_siren' : str,
+                              u'c_nic' : str,
+                              u'c_siret' : str},
+                     parse_dates = [u'date_ouv', u'date_fer', u'date_reouv',
+                                    u'date_chg_enseigne', u'date_chg_surface'],
                      encoding = 'UTF-8')
 
 # MERGE
-df_stores = pd.merge(df_lsa[['Ident', 'Latitude', 'Longitude', 'Enseigne_Alt', 'Groupe']],
+df_stores = pd.merge(df_lsa[['id_lsa',
+                             'latitude',
+                             'longitude',
+                             'enseigne_alt',
+                             'groupe']],
                      df_stores,
-                     left_on = 'Ident',
+                     left_on = 'id_lsa',
                      right_on = 'id_lsa',
                      how = 'right')
 
-df_stores.sort(['Period', 'Store'],
+df_stores.sort(['period', 'store'],
                ascending = True,
                inplace = True)
 
@@ -61,9 +65,9 @@ df_stores.sort(['Period', 'Store'],
 df_stores = df_stores[~df_stores['id_lsa'].isnull()]
 # check no duplicate (period, lsa_id) => should not be...
 print u'\nCheck no duplicates (should be empty):'
-print df_stores[df_stores.duplicated(['Period', 'id_lsa'])]
+print df_stores[df_stores.duplicated(['period', 'id_lsa'])]
 # drop in case there are...
-df_stores = df_stores.drop_duplicates(['Period', 'id_lsa'])
+df_stores = df_stores.drop_duplicates(['period', 'id_lsa'])
 
 # #####################
 # GET CLOSE STORE PAIRS
@@ -71,65 +75,66 @@ df_stores = df_stores.drop_duplicates(['Period', 'id_lsa'])
 
 print '\nGetting list of qlmc close pairs:'
 ls_close_pairs  = []
-for Period in df_stores['Period'].drop_duplicates():
-  df_stores_per = df_stores[df_stores['Period'] == Period]
+for period in df_stores['period'].drop_duplicates():
+  df_stores_per = df_stores[df_stores['period'] == period]
   for row_i, row in df_stores_per.iterrows():
-    ident, lat, lng = row[['Ident', 'Latitude', 'Longitude']].values
+    ident, lat, lng = row[['id_lsa', 'latitude', 'longitude']].values
     # want list of unique pairs
     df_temp = df_stores_per.ix[row_i:][1:].copy() # [1:] to avoid itself
     df_temp['lat_temp'] = lat
     df_temp['lng_temp'] = lng
-    df_temp['dist'] = compute_distance_ar(df_temp['Latitude'],
-                                          df_temp['Longitude'],
+    df_temp['dist'] = compute_distance_ar(df_temp['latitude'],
+                                          df_temp['longitude'],
                                           df_temp['lat_temp'],
                                           df_temp['lng_temp'])
-    ls_close = df_temp[df_temp['dist'] <= 25][['Ident', 'dist']].values.tolist()
-    ls_close_pairs += [[int(Period), ident, ident_close, dist]\
+    ls_close = df_temp[df_temp['dist'] <= 25][['id_lsa', 'dist']].values.tolist()
+    ls_close_pairs += [[int(period), ident, ident_close, dist]\
                              for (ident_close, dist) in ls_close]
 
 df_close_pairs = pd.DataFrame(ls_close_pairs,
-                              columns = ['Period', 'id_lsa_0', 'id_lsa_1', 'dist'])
+                              columns = ['period', 'id_lsa_0', 'id_lsa_1', 'dist'])
 
 dict_close_pairs = {}
-for Period, ident, ident_close, dist in ls_close_pairs:
-  dict_close_pairs.setdefault(ident, []).append([Period, ident_close, dist])
-  dict_close_pairs.setdefault(ident_close, []).append([Period, ident, dist])
+for period, ident, ident_close, dist in ls_close_pairs:
+  dict_close_pairs.setdefault(ident, []).append([period, ident_close, dist])
+  dict_close_pairs.setdefault(ident_close, []).append([period, ident, dist])
 
 
 # ################################
 # ADD Store AND Store_Chain IN DF
 # ################################
 
-# todo: take Enseigne_Alt / Groupe at the right period
+# todo: take enseigne_alt / groupe at the right period
 # suffix works only with redundant columns
 
-df_stores = df_stores[['Period', 'Store', 'Store_Chain', 'Store_Municipality',
-                       'id_lsa', 'Enseigne_Alt', 'Groupe']]
-df_stores.columns = ['{:s}_0'.format(x) if x != 'Period' else x for x in df_stores.columns]
+df_stores = df_stores[['period', 'store', 'store_chain', 'store_municipality',
+                       'id_lsa', 'enseigne_alt', 'groupe']]
+df_stores.columns = ['{:s}_0'.format(x) if x != 'period'\
+                                        else x for x in df_stores.columns]
 
 df_close_pairs = pd.merge(df_close_pairs,
                           df_stores,
-                          on = ['Period', 'id_lsa_0'],
+                          on = ['period', 'id_lsa_0'],
                           how = 'left')
 
 df_stores.columns = [x.replace('0', '1') for x in df_stores.columns]
 
 df_close_pairs = pd.merge(df_close_pairs,
                           df_stores,
-                          on = ['Period', 'id_lsa_1'],
+                          on = ['period', 'id_lsa_1'],
                           how = 'left')
 
 # #############################################
 # GET COMP STORE PAIRS & SAME CHAIN STORE PAIRS
 # #############################################
 
-df_comp_pairs = df_close_pairs[df_close_pairs['Groupe_0'] !=\
-                               df_close_pairs['Groupe_1']]
+df_comp_pairs = df_close_pairs[df_close_pairs['groupe_0'] !=\
+                               df_close_pairs['groupe_1']]
 
-df_same_pairs = df_close_pairs[df_close_pairs['Groupe_0'] ==\
-                               df_close_pairs['Groupe_1']]
+df_same_pairs = df_close_pairs[df_close_pairs['groupe_0'] ==\
+                               df_close_pairs['groupe_1']]
 
-# May wish to add Surface
+# May wish to add surface
 
 # #############
 # OUTPUT
--- code_supermarkets_france/data_acquisition/formatting_qlmc_2007-12_pdfs/build_competition/build_df_pairs_compared.py
+++ code_supermarkets_france/data_acquisition/formatting_qlmc_2007-12_pdfs/build_competition/build_df_pairs_compared.py
@@ -36,76 +36,76 @@ df_prices = pd.read_csv(os.path.join(path_built_csv,
 #Period = 5
 ls_df_repro_compa = []
 ls_ls_prod_draws = []
-for Period in range(9)[0:3]:
-  df_comp_pairs_per = df_comp_pairs[df_comp_pairs['Period'] == Period]
-  df_prices_per = df_prices[df_prices['Period'] == Period]
+for period in range(9)[0:3]:
+  df_comp_pairs_per = df_comp_pairs[df_comp_pairs['period'] == period]
+  df_prices_per = df_prices[df_prices['period'] == period]
   ls_rows_compa = []
   ls_prod_draws = []
   store_0_save = None
   for row_i, row in df_comp_pairs_per.iterrows():
-    store_0 = row['Store_0']
-    store_1 = row['Store_1']
+    store_0 = row['store_0']
+    store_1 = row['store_1']
     dist = row ['dist']
     same_municipality = 0
-    if row['Store_Municipality_0'] == row['Store_Municipality_1']:
+    if row['store_municipality_0'] == row['store_municipality_1']:
       same_municipality = 1
     # taking advantage of order requires caution on first loop
     if store_0 != store_0_save:
-      df_prices_0 = df_prices_per[df_prices_per['Store'] == store_0]
-      store_0_save = df_prices_per['Store'].iloc[0]
-    df_prices_1 = df_prices_per[df_prices_per['Store'] == store_1]
+      df_prices_0 = df_prices_per[df_prices_per['store'] == store_0]
+      store_0_save = df_prices_per['store'].iloc[0]
+    df_prices_1 = df_prices_per[df_prices_per['store'] == store_1]
     
     # todo: see if need family and subfamily for matching (not much chge)
     df_duel = pd.merge(df_prices_0,
                        df_prices_1,
                        how = 'inner',
-                       on = ['Family', 'Subfamily', 'Product'],
+                       on = ['section', 'family', 'product'],
                        suffixes = ['_0', '_1'])
-    mean_diff = (df_duel['Price_0'] - df_duel['Price_1']).mean()
+    mean_diff = (df_duel['price_0'] - df_duel['price_1']).mean()
     # mean saving
-    if len(df_duel[df_duel['Price_0'] < df_duel['Price_1']]) >\
-         len(df_duel[df_duel['Price_0'] > df_duel['Price_1']]):
-       df_rr = df_duel[df_duel['Price_1'] < df_duel['Price_0']]
-       mean_rr_saving = (df_rr['Price_0'] - df_rr['Price_1']).mean()
+    if len(df_duel[df_duel['price_0'] < df_duel['price_1']]) >\
+         len(df_duel[df_duel['price_0'] > df_duel['price_1']]):
+       df_rr = df_duel[df_duel['price_1'] < df_duel['price_0']]
+       mean_rr_saving = (df_rr['price_0'] - df_rr['price_1']).mean()
     else:
-       df_rr = df_duel[df_duel['Price_0'] < df_duel['Price_1']]
-       mean_rr_saving = (df_rr['Price_1'] - df_rr['Price_0']).mean()
-    # df_duel['Price_0_ctld'] = df_duel['Price_0'] - mean_diff
-    ls_rows_compa.append((Period,
+       df_rr = df_duel[df_duel['price_0'] < df_duel['price_1']]
+       mean_rr_saving = (df_rr['price_1'] - df_rr['price_0']).mean()
+    # df_duel['price_0_ctld'] = df_duel['price_0'] - mean_diff
+    ls_rows_compa.append((period,
                           store_0,
                           store_1,
                           dist,
                           same_municipality,
                           len(df_duel),
-                          len(df_duel[df_duel['Price_0'] < df_duel['Price_1']]),
-                          len(df_duel[df_duel['Price_0'] > df_duel['Price_1']]),
-                          len(df_duel[df_duel['Price_0'] == df_duel['Price_1']]),
-                          df_duel[['Price_0', 'Price_1']].sum().argmin(),
-                          np.abs((df_duel[['Price_0', 'Price_1']].sum().min() /\
-                                    df_duel[['Price_0', 'Price_1']].sum().max() - 1)* 100),
+                          len(df_duel[df_duel['price_0'] < df_duel['price_1']]),
+                          len(df_duel[df_duel['price_0'] > df_duel['price_1']]),
+                          len(df_duel[df_duel['price_0'] == df_duel['price_1']]),
+                          df_duel[['price_0', 'price_1']].sum().argmin(),
+                          np.abs((df_duel[['price_0', 'price_1']].sum().min() /\
+                                    df_duel[['price_0', 'price_1']].sum().max() - 1)* 100),
                           mean_diff,
-                          (df_duel['Price_0'] - df_duel['Price_1']).abs().mean(),
+                          (df_duel['price_0'] - df_duel['price_1']).abs().mean(),
                           mean_rr_saving))
 
     # Save product names when equal prices
-    ls_prod_draws += list(df_duel[df_duel['Price_0'] == df_duel['Price_1']]['Product'].values)
+    ls_prod_draws += list(df_duel[df_duel['price_0'] == df_duel['price_1']]['product'].values)
   
   # Pbm: pct compa does not make sense: todo do always cheapest / most expensive
   df_repro_compa = pd.DataFrame(ls_rows_compa,
-                                columns = ['Period',
-                                           'Store_0',
-                                           'Store_1',
-                                           'Dist',
-                                           'Same_municipality',
-                                           'Nb_obs',
-                                           'Nb_0_wins',
-                                           'Nb_1_wins',
-                                           'Nb_draws',
-                                           'Winner_value',
-                                           'Winner_value_pct',
-                                           'Mean_diff',
-                                           'Mean_abs_gfs',
-                                           'Mean_rr_saving'])
+                                columns = ['period',
+                                           'store_0',
+                                           'store_1',
+                                           'dist',
+                                           'same_municipality',
+                                           'nb_obs',
+                                           'nb_0_wins',
+                                           'nb_1_wins',
+                                           'nb_draws',
+                                           'winner_value',
+                                           'winner_value_pct',
+                                           'mean_diff',
+                                           'mean_abs_gfs',
+                                           'mean_rr_saving'])
   ls_df_repro_compa.append(df_repro_compa)
   ls_ls_prod_draws.append(ls_prod_draws)
 
@@ -117,16 +117,16 @@ df_compa_all_periods = pd.concat(ls_df_repro_compa,
                                  axis = 0,
                                  ignore_index = True)
 
-df_compa_all_periods['Rank_rev'] = df_compa_all_periods['Nb_1_wins'] /\
-                               df_compa_all_periods['Nb_obs'] * 100
+df_compa_all_periods['rank_rev'] = df_compa_all_periods['nb_1_wins'] /\
+                               df_compa_all_periods['nb_obs'] * 100
 
-df_compa_all_periods.loc[df_compa_all_periods['Nb_1_wins'] >\
-                     df_compa_all_periods['Nb_0_wins'],
-                   'Rank_rev'] = df_compa_all_periods['Nb_0_wins'] /\
-                                   df_compa_all_periods['Nb_obs'] * 100
+df_compa_all_periods.loc[df_compa_all_periods['nb_1_wins'] >\
+                     df_compa_all_periods['nb_0_wins'],
+                   'rank_rev'] = df_compa_all_periods['nb_0_wins'] /\
+                                   df_compa_all_periods['nb_obs'] * 100
 
-df_compa_all_periods.loc[df_compa_all_periods['Nb_obs'] == 0,
-                         'Rank_rev'] = np.nan
+df_compa_all_periods.loc[df_compa_all_periods['nb_obs'] == 0,
+                         'rank_rev'] = np.nan
 
 #df_compa_all_periods.to_csv(os.path.join(path_built_csv,
 #                                         'df_compa_all_periods.csv'),
@@ -144,21 +144,21 @@ pd.set_option('float_format', '{:,.3f}'.format)
 ls_pctiles = [0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95]
 
 print u'\nOverview of winner value pct:'
-print df_repro_compa['Winner_value_pct'].describe(percentiles = ls_pctiles)
+print df_repro_compa['winner_value_pct'].describe(percentiles = ls_pctiles)
 
-df_repro_compa['Rank_rev'] = df_repro_compa['Nb_1_wins'] /\
-                               df_repro_compa['Nb_obs'] * 100
+df_repro_compa['rank_rev'] = df_repro_compa['nb_1_wins'] /\
+                               df_repro_compa['nb_obs'] * 100
 
-df_repro_compa.loc[df_repro_compa['Nb_1_wins'] >\
-                     df_repro_compa['Nb_0_wins'],
-                   'Rank_rev'] = df_repro_compa['Nb_0_wins'] /\
-                                   df_repro_compa['Nb_obs'] * 100
+df_repro_compa.loc[df_repro_compa['nb_1_wins'] >\
+                     df_repro_compa['nb_0_wins'],
+                   'rank_rev'] = df_repro_compa['nb_0_wins'] /\
+                                   df_repro_compa['nb_obs'] * 100
 
 print u'\nGeneral overview'
 print df_repro_compa.describe().to_string()
 
 import matplotlib.pyplot as plt
-df_repro_compa.plot(kind = 'scatter', x = 'Winner_value_pct', y = 'Rank_rev')
+df_repro_compa.plot(kind = 'scatter', x = 'winner_value_pct', y = 'rank_rev')
 plt.show()
 
 # Check if some products are prone to draws? how to do efficiently?
@@ -166,18 +166,18 @@ plt.show()
 
 # Todo: add from LSA: Enseigne_Alt and Group to have correct df_comp and df_same
 
-print smf.ols('Rank_rev ~ Winner_value_pct + Dist',
+print smf.ols('rank_rev ~ winner_value_pct + dist',
         data = df_repro_compa).fit().summary()
 
-df_repro_compa['Close'] = 0
-df_repro_compa.loc[df_repro_compa['Dist'] <= 5, 'Close'] = 1
+df_repro_compa['close'] = 0
+df_repro_compa.loc[df_repro_compa['dist'] <= 5, 'close'] = 1
 
-print df_repro_compa[['Close', 'Same_municipality']].describe()
+print df_repro_compa[['close', 'same_municipality']].describe()
 
-print smf.ols('Rank_rev ~ Winner_value_pct + Close',
+print smf.ols('rank_rev ~ winner_value_pct + close',
         data = df_repro_compa).fit().summary()
 
-print smf.ols('Rank_rev ~ Winner_value_pct + Same_municipality',
+print smf.ols('rank_rev ~ winner_value_pct + same_municipality',
         data = df_repro_compa).fit().summary()
 
 # Product draw
