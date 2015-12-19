afc4f371f174a41e63cf58e8a0ae2db83d21cdbc
etna <echamayou@gmail.com>
2015-12-19 10:51:48 +0100

    update zagaz

--- code_gasoline_france/analysis/analysis_zagaz/stations/highway.py
+++ code_gasoline_france/analysis/analysis_zagaz/stations/highway.py
@@ -78,28 +78,6 @@ df_zagaz_info.set_index('id_zagaz', inplace = True)
 # todo: drop dup in zagaz
 df_zagaz_info = df_zagaz_info[df_zagaz_info.index != '19674']
 
-# #################
-# DF HIGHWAY GOV
-# #################
-
-# A1, A4, A6, A7, A8, A9, A10, A26, A31, A43 done
-str_hw = '63'
-
-df_hw_gov = df_info[df_info['highway'] == 1].copy()
-
-lsdhwgov = ['name', 'adr_street', 'adr_city', 'ci_1', 'brand_0', 'brand_1']
-
-print()
-print(u'Overview of gov highway stations')
-print(df_hw_gov[lsdhwgov][0:20].to_string())
-
-print()
-print(u'Overview of gov A{:s} stations'.format(str_hw))
-print(df_hw_gov[df_hw_gov['adr_street'].str.contains('A\s?{:s}(?:\s|$|-)'.format(str_hw))]\
-               [lsdhwgov].to_string())
-
-#print(df_hw_gov[df_hw_gov['ci_1'] == '89438'][lsdhwgov].to_string())
-#print(df_hw_gov[df_hw_gov['dpt'] == '89'][lsdhwgov].to_string())
 
 # #################
 # DF HIGHWAY ZAGAZ
@@ -107,9 +85,11 @@ print(df_hw_gov[df_hw_gov['adr_street'].str.contains('A\s?{:s}(?:\s|$|-)'.format
 
 df_hw_zag = df_zagaz_info[~df_zagaz_info['highway'].isnull()].copy()
 
-# Temp fix
-df_hw_zag.loc['7318', 'highway'] =\
-  df_hw_zag.loc['7318', 'highway'].replace('Paris / Strasbourg', 'Strasbourg / Paris')
+# Temp fixes
+ls_fixes = [['7318', u'Autoroute A4 - Strasbourg / Paris (km 358)'],
+            ['5229', u'Autoroute A63 - Bordeaux / Hendaye (km 0)']]
+for id_zagaz, str_highway in ls_fixes:
+  df_hw_zag.loc[id_zagaz, 'highway'] = str_highway
 
 # Match zagaz w/ gov id (can result in duplicates: inspect and fix)
 df_hw_zag = pd.merge(df_hw_zag,
@@ -150,11 +130,50 @@ print(u'Inspect duplicates')
 df_hw_zag['dup'] = df_hw_zag.groupby('zag_id')['zag_id'].transform(len)
 print(df_hw_zag[df_hw_zag['dup'] > 1][lsdhw].to_string(index = False))
 
-# Check highways
+# Inspect zagaz highways
+
+# Checked:
+# A1, A4, A6, A7, A8, A9, A10, A13, A26, A31, A36, A43, A61, A71, A83, A64, A89
+
+#ls_str_hw = ['61', '83', '64']
+ls_str_hw = [x[1:] for x in df_hw_zag['hw_id'].value_counts()[0:40].index.tolist()\
+                   if x[0] == 'A']
+ls_other_hw = [x for x in df_hw_zag['hw_id'].value_counts()[0:40].index.tolist()\
+                  if x[0] != 'A']
+
 df_hw_zag.sort(['hw_id', 'hw_dir', 'hw_km'], ascending = True, inplace = True)
+for str_hw in ls_str_hw:
+  print()
+  print(u'Inspect highway: {:s}'.format(str_hw))
+  print(df_hw_zag[df_hw_zag['hw_id'] == 'A{:s}'.format(str_hw)][lsdhw].to_string(index = False))
+
+# #################
+# DF HIGHWAY GOV
+# #################
+
+df_hw_gov = df_info[df_info['highway'] == 1].copy()
+
+lsdhwgov = ['name', 'adr_street', 'adr_city', 'ci_1', 'brand_0', 'brand_1']
+
 print()
-print(u'Inspect highways')
-print(df_hw_zag[df_hw_zag['hw_id'] == 'A{:s}'.format(str_hw)][lsdhw].to_string(index = False))
+print(u'Overview of gov highway stations')
+print(df_hw_gov[lsdhwgov][0:20].to_string())
+
+for str_hw in ls_str_hw:
+  print()
+  print(u'Overview of gov A{:s} highway stations'.format(str_hw))
+  print(df_hw_gov[df_hw_gov['adr_street'].str.contains('A\s?{:s}(?:\s|$|-)'.format(str_hw))]\
+                 [lsdhwgov].to_string())
+
+#print(df_hw_gov[df_hw_gov['ci_1'] == '89438'][lsdhwgov].to_string())
+#print(df_hw_gov[df_hw_gov['dpt'] == '89'][lsdhwgov].to_string())
+
+# #################
+# MANUAL MATCHING
+# #################
+
+# len(df_hw_zag[df_hw_zag['gov_id'].isnull()])
+# Only 22 left in zag, check from gov side
 
 ls_match = [('80200010', '10791'), # A1
             ('80200005', '13343'),
@@ -230,18 +249,96 @@ ls_match = [('80200010', '10791'), # A1
             ('2690001' ,   '234'),
             ('62860005',  '8221'),
             ('10150003', '16712'),
-            ('64210002',  '8476'), # A 63
+            ('64210002',  '8476'), # A63
             ('64210001',  '8477'),
             ('40530001',  '5150'),
-            ('40530002',  '5151')]
+            ('40530002',  '5151'),
+            ('33610004', '15886'), # check..
+            ('33610006', '17001'),
+            ('40410004',  '5229'),
+            ('63190002',  '8385'), # A89
+            ('63190010',  '8373'), # check if same '63190003' 
+            ('42440008',  '5409'),
+            ('42440006',  '5408'), # not 100% sure which is which
+            ('3100006' , '17434'), # A71
+            ('3170001' , '17604'),
+            ('21250009',  '2162'), # A36
+            ('21250005', '16213'),
+            ('39700011',  '5092'),
+            ('31290003', '16015'), # A61
+            ('31290002',  '3564'),
+            ('11290002',   '725'),
+            ('11290001',   '726'),
+            ('85210007', '11586'), # A83
+            ('64170009',  '8502'), # A 64
+            ('64170008', '15429'),
+            ('31410003', '15494'),
+            ('31410002',  '3587'),
+            ('78630002', '13433'), # A13
+            ('78630003', '13435'),
+            ('78710003', '13434'),
+            ('78710002', '12952'),
+            ('61230001', '16080'), # A28
+            ('80140002', '10835'),
+            ('80140001', '10836'),
+            ('89190005', '12116'), # A5 (not 100%)
+            ('89190004', '12118'),
+            ('10270002',   '669'),
+            ('10270001',   '670'),
+            ('77130009', '13427'),
+            ('77130010', '10482'),
+            ('13320001',   '979'), # A51
+            ('4130003',    '402'),
+            ('4130002',    '403'),
+            ('4200006',    '348'),
+            ('94000002', '13714'), # A86
+            ('94003001', '12462'),
+            ('94150001', '12513'), # todo: fix dum highway
+            ('94150002', '12514'),
+            ('62250004',  '8201'), # A16
+            ('12150001',   '916'), # A75
+            ('49160003', '13087'), # A85
+            ('49160004', '13086'),
+            ('37190003', '20120'),
+            ('59242001', '14750'), # A23
+            ('59242002', '14749'),
+            ('59494008', '14752'),
+            ('59494003',  '7714'),
+            ('68390001',  '8933'), # A35
+            ('67600003', '13689'),
+            ('21130005',  '2191'), # A39
+            ('21130006',  '2192'),
+            ('73100007',  '9815'), # A41
+            ('62147001',  '8159'), # A2
+            ('62147003',  '8148'),
+            ('33520003',  '3921'), # Rocade A630
+            ('33520002',  '3922'),
+            ('33700008', '14512'), # todo: fix dum highway?
+            ('33700007',  '4049'),
+            ('33170003', '13672'),
+            ('33170004',  '3967'),
+            ('33310002', '14728'), # todo: fix dum highway?
+            ('91100007', '13196'), # Francil N104
+            ('91250009', '13195'), # todo: fix dum highway?
+            ('91460011', '13017'),
+            ('91460010', '13018'),
+            ('75012008', '10085'), # Periph: check + highway?
+            ('75012013', '10086'),
+            ('75018003', '10173'),
+            ('1390004' , '13279'), # Lyon Est A 46
+            ('1390006' , '13280'),
+            ('69360005',  '9110'),
+            ('69360004',  '9109')]
 
-# todo (if not done yet):
-# drop '62128009' (temp dup of '62128004')
-# drop '76170004' (temp dup of '76210003')
-# drop '41000015' (ctd dup of '41000006')
-# drop '45190008' (temp dup of '45190004')
-# drop '85210005', '85210006' (almost no info)
-# drop '87160005', '87160006' (almost no info)
+# not found in gouv '983', '15160'
+                            
+# todo (if not done yet):   
+# drop '62128009' (temp dup  of '62128004')
+# drop '76170004' (temp dup  of '76210003')
+# drop '41000015' (ctd dup  of '41000006')
+# drop '45190008' (temp dup  of '45190004')
+# drop '85210005', '8521000 6' (almost no info)
+# drop '87160005', '8716000 6' (almost no info)
 # drop '34400014' (temp dup of '34400016')
 # drop '44370007' (temp dup of '44370004')
 # drop '38080005' (temp dup of '38080004')
