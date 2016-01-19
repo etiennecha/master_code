3cc14880f1342836ddf254c7ba00b4921ed4574e
etiennecha <echamayou@gmail.com>
2016-01-19 10:02:08 +0100

    rename pair dispersion analysis scripts

--- code_gasoline_france/analysis/analysis_dispersion/pairs/overview_likely_competitors.py
+++ code_gasoline_france/analysis/analysis_dispersion/pairs/overview_likely_competitors.py
@@ -1,45 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 import add_to_path
 from add_to_path import path_data
 from generic_master_price import *
 from generic_master_info import *
 from generic_competition import *
 import time
-from statsmodels.distributions.empirical_distribution import ECDF
-from scipy.stats import ks_2samp
 
-pd.set_option('float_format', '{:,.3f}'.format)
+path_dir_built = os.path.join(path_data,
+                              u'data_gasoline',
+                              u'data_built',
+                              u'data_scraped_2011_2014')
+path_dir_built_csv = os.path.join(path_dir_built, u'data_csv')
+path_dir_built_json = os.path.join(path_dir_built, u'data_json')
+path_dir_built_graphs = os.path.join(path_dir_built, u'data_graphs')
+
+path_dir_built_dis = os.path.join(path_data,
+                                  u'data_gasoline',
+                                  u'data_built',
+                                  u'data_dispersion')
+path_dir_built_dis_csv = os.path.join(path_dir_built_dis, u'data_csv')
+path_dir_built_dis_json = os.path.join(path_dir_built_dis, u'data_json')
+
+pd.set_option('float_format', '{:,.2f}'.format)
 format_float_int = lambda x: '{:10,.0f}'.format(x)
-format_float_float = lambda x: '{:10,.3f}'.format(x)
+format_float_float = lambda x: '{:10,.2f}'.format(x)
 
-# ###################
+# ################
 # LOAD DATA
-# ###################
+# ################
 
-path_dir_built_paper = os.path.join(path_data,
-                                    'data_gasoline',
-                                    'data_built',
-                                    'data_paper_dispersion')
-
-path_dir_built_csv = os.path.join(path_dir_built_paper, u'data_csv')
-path_dir_built_json = os.path.join(path_dir_built_paper, 'data_json')
-
-path_dir_source = os.path.join(path_data, 'data_gasoline', 'data_source')
-
-# LOAD DF PRICES
-df_prices_ttc = pd.read_csv(os.path.join(path_dir_built_csv,
-                                         'df_prices_ttc_final.csv'),
-                        parse_dates = ['date'])
-df_prices_ttc.set_index('date', inplace = True)
-
-df_prices_cl = pd.read_csv(os.path.join(path_dir_built_csv,
-                                        'df_cleaned_prices.csv'),
-                          parse_dates = ['date'])
-df_prices_cl.set_index('date', inplace = True)
-
-# LOAD DF INFO
+# DF INFO
 df_info = pd.read_csv(os.path.join(path_dir_built_csv,
                                    'df_station_info_final.csv'),
                       encoding = 'utf-8',
@@ -53,21 +46,61 @@ df_info = pd.read_csv(os.path.join(path_dir_built_csv,
                                'dpt' : str})
 df_info.set_index('id_station', inplace = True)
 
-# LOAD DF STATION STATS
-print '\nLoad df_station_stats'
+# DF STATION STATS
 df_station_stats = pd.read_csv(os.path.join(path_dir_built_csv,
                                             'df_station_stats.csv'),
                                encoding = 'utf-8',
                                dtype = {'id_station' : str})
 df_station_stats.set_index('id_station', inplace = True)
 
-# LOAD DF PAIRS
-print '\nLoad df_ppd'
+# CLOSE STATIONS
+dict_ls_close = dec_json(os.path.join(path_dir_built_json,
+                                      'dict_ls_close.json'))
+
+# DF PRICES
+df_prices_ttc = pd.read_csv(os.path.join(path_dir_built_csv,
+                                         'df_prices_ttc_final.csv'),
+                        parse_dates = ['date'])
+df_prices_ttc.set_index('date', inplace = True)
+
+df_prices_ht = pd.read_csv(os.path.join(path_dir_built_csv,
+                                        'df_prices_ht_final.csv'),
+                        parse_dates = ['date'])
+df_prices_ht.set_index('date', inplace = True)
+
+df_prices_cl = pd.read_csv(os.path.join(path_dir_built_csv,
+                                        'df_cleaned_prices.csv'),
+                          parse_dates = True)
+df_prices_cl.set_index('date', inplace = True)
+
+# FILTER DATA
+# exclude stations with insufficient (quality) price data
+df_filter = df_station_stats[~((df_station_stats['pct_chge'] < 0.03) |\
+                               (df_station_stats['nb_valid'] < 90))]
+ls_keep_ids = list(set(df_filter.index).intersection(\
+                     set(df_info[(df_info['highway'] != 1) &\
+                                 (df_info['reg'] != 'Corse')].index)))
+#df_info = df_info.ix[ls_keep_ids]
+#df_station_stats = df_station_stats.ix[ls_keep_ids]
+#df_prices_ttc = df_prices_ttc[ls_keep_ids]
+#df_prices_ht = df_prices_ht[ls_keep_ids]
+#df_prices_cl = df_prices_cl[ls_keep_ids]
+
+ls_drop_ids = list(set(df_prices_ttc.columns).difference(set(ls_keep_ids)))
+df_prices_ttc[ls_drop_ids] = np.nan
+df_prices_ht[ls_drop_ids] = np.nan
+# highway stations may not be in df_prices_cl (no pbm here)
+ls_drop_ids_nhw =\
+  list(set(ls_drop_ids).difference(set(df_info[df_info['highway'] == 1].index)))
+df_prices_cl[ls_drop_ids_nhw] = np.nan
+
+# DF PAIRS
+
 ls_dtype_temp = ['id', 'ci_ardt_1']
 dict_dtype = dict([('%s_1' %x, str) for x in ls_dtype_temp] +\
                   [('%s_2' %x, str) for x in ls_dtype_temp])
-df_pairs = pd.read_csv(os.path.join(path_dir_built_csv,
-                     'df_pairs.csv'),
+df_pairs = pd.read_csv(os.path.join(path_dir_built_dis_csv,
+                                    'df_pair_final.csv'),
               encoding = 'utf-8',
               dtype = dict_dtype)
 
@@ -141,4 +174,4 @@ ls_cols = ['id_station',
 df_lcomp = pd.DataFrame(ls_rows, columns = ls_cols)
 df_lcomp.set_index('id_station', inplace = True)
 
-print df_lcomp.describe().to_string()
+print(df_lcomp.describe().to_string())
