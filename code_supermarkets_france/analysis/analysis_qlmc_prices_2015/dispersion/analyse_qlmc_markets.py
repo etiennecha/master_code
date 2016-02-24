5cc4452df1bf79f671a26b38818a6717f687e9b3
etna <echamayou@gmail.com>
2016-02-24 09:30:00 +0100

    update market analysis

--- code_supermarkets_france/analysis/analysis_qlmc_prices_2015/dispersion/analyse_qlmc_markets.py
+++ code_supermarkets_france/analysis/analysis_qlmc_prices_2015/dispersion/analyse_qlmc_markets.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 import add_to_path
 from add_to_path import *
 import os, sys
-import httplib
-import urllib, urllib2
-from bs4 import BeautifulSoup
-import re
-import json
-import pandas as pd
-from functions_generic_qlmc import *
 import numpy as np
+import pandas as pd
+import timeit
 
 pd.set_option('float_format', '{:,.2f}'.format)
 format_float_int = lambda x: '{:10,.0f}'.format(x)
@@ -24,88 +20,86 @@ path_built_csv = os.path.join(path_data,
                               'data_csv_201503')
 
 df_prices = pd.read_csv(os.path.join(path_built_csv,
-                                     'df_prices.csv'))
+                                     'df_prices.csv'),
+                        encoding = 'utf-8')
 
 df_stores = pd.read_csv(os.path.join(path_built_csv,
-                                     'df_stores_final.csv'))
+                                     'df_stores_final.csv'),
+                        dtype = {'id_lsa' : str,
+                                 'c_insee' : str},
+                        encoding = 'utf-8')
 
-df_comp = pd.read_csv(os.path.join(path_built_csv,
-                                   'df_qlmc_competitors.csv'))
+df_qlmc_comparisons = pd.read_csv(os.path.join(path_built_csv,
+                                               'df_qlmc_competitors.csv'),
+                                  encoding = 'utf-8')
 
-# CORSE
+# Costly to search by store_id within df_prices
+# hence first split df_prices in chain dataframes
+dict_chain_dfs = {chain: df_prices[df_prices['store_chain'] == chain]\
+                    for chain in df_prices['store_chain'].unique()}
 
-# TEMP: define market
-ls_corse_ids = ['centre-e-leclerc-corbara',
-                'centre-e-leclerc-ajaccio-rte-d-alata',
-                'centre-e-leclerc-ajaccio-prince-imperial',
-                'centre-e-leclerc-porto-vecchio',
-                'centre-e-leclerc-ghisonaccia',
-                'centre-e-leclerc-aleria',
-                'centre-e-leclerc-san-giuliano',
-                'centre-e-leclerc-oletta',
-                'leclerc-express-borgo',
-                'centre-e-leclerc-bastia']
+ls_keep_stores = df_prices['store_id'].unique()
+dict_markets = {}
+for i, row in df_qlmc_comparisons.iterrows():
+  if (row['lec_id'] in ls_keep_stores) and\
+     (row['comp_id'] in ls_keep_stores):
+    dict_markets.setdefault(row['lec_id'], []).append(row['comp_id'])
 
-# TEMP: keep only stores present in data
-ls_collected_store_ids = df_prices['store_id'].unique().tolist()
-ls_market_ids = [store_id for store_id in ls_corse_ids\
-                   if store_id in ls_collected_store_ids]
+# see if quick enough without using dict_chain_dfs
+ls_df_markets = []
+for lec_id, ls_comp_id in dict_markets.items()[0:10]:
+  if ls_comp_id:
+    df_market = df_prices[df_prices['store_id'] == lec_id]\
+                      [['section', 'family', 'product', 'price']]
+    for comp_id in ls_comp_id:
+      df_comp = df_prices[df_prices['store_id'] == comp_id]
+      df_market = pd.merge(df_market,
+                           df_comp[['section', 'family', 'product', 'price']],
+                           on = ['section', 'family', 'product'],
+                           suffixes = ('', '_{:s}'.format(comp_id)),
+                           how = 'inner')
+    # do before?
+    df_market.set_index(['section', 'family', 'product'], inplace = True)
+    df_market.rename(columns = {'price' : 'price_{:s}'.format(lec_id)},
+                     inplace = True)
+    df_market.columns = [x[6:] for x in df_market.columns] # get rid of 'price_'
+    ls_df_markets.append(df_market)
 
-# Merge store prices (inner for now... but intersection quickly reduces)
-df_market = df_prices[df_prices['store_id'] == ls_market_ids[0]]\
-                     [['section', 'family', 'product' ,'price']]
-for store_id in ls_market_ids[1:]:
-  df_market = pd.merge(df_market,
-                       df_prices[df_prices['store_id'] == store_id][['product', 'price']],
-                       on = 'product',
-                       suffixes = ('', '_{:s}'.format(store_id)),
-                       how = 'inner')
-df_market.rename(columns = {'price' : 'price_{:s}'.format(ls_market_ids[0])}, inplace = True)
+# Aggregate stats: basket with all products
+nb_products = len(df_market) # check enough products
+agg_sum = df_market.sum() # dispersion in total sum
+agg_range = agg_sum.max() - agg_sum.min()
+agg_range_pct = agg_range / agg_sum.mean()
+agg_gfs = agg_sum.mean() - agg_sum.min()
+agg_gfs_pct = agg_gfs / agg_sum.mean()
 
-df_market_pr = df_market.drop(['family', 'section'], axis = 1)
-df_market_pr.set_index('product', inplace = True)
-df_market_pr = df_market_pr.T
+# Dispersion by product
+ls_cols = df_market.columns # keep cuz gona add columns
+df_market['mean'] = df_market.mean(1)
+df_market['range'] = df_market[ls_cols].max(1) - df_market[ls_cols].min(1)
+df_market['range_pct'] = df_market['range'] / df_market[ls_cols].mean(1)
+df_market['gfs'] = df_market[ls_cols].mean(1) - df_market[ls_cols].min(1)
+df_market['gfs_pct'] = df_market['gfs'] / df_market[ls_cols].mean(1)
+df_market['std'] = df_market[ls_cols].std(1)
+df_market['cv'] = df_market[ls_cols].std(1) / df_market[ls_cols].mean(1)
 
-se_sum = df_market_pr.sum(1)
-print se_sum.to_string()
+print()
+print('Product dispersion summary stats')
+print(df_market[['mean', 'range', 'range_pct', 'gfs', 'gfs_pct', 'std', 'cv']].describe())
 
-df_market_su = df_market_pr.describe().T
-df_market_su['spread'] = df_market_su['max'] - df_market_su['min']
-df_market_su['gfs'] = df_market_su['mean'] - df_market_su['min']
-df_market_su['cv'] = df_market_su['std'] / df_market_su['mean']
+# Ranking within market?
+# http://stackoverflow.com/questions/21188151/pandas-getting-the-name-of-the-minimum-column
+df_market['cheapest'] = df_market[ls_cols].T.idxmin()
+df_market['priciest'] = df_market[ls_cols].T.idxmax()
 
-# Compare leclerc stores which offer those same products in mainland France
-# (efficient? better ways to do? => simple reg?)
-# (dispersion in mainland france btw?)
+print()
+print('Cheapest store (basket share)')
+print(df_market['cheapest'].value_counts(normalize = 1))
 
-## FRANCE MAINLAND (pbm: not one product avail in all stores!)
-#
-#ls_france_ids = df_stores[(df_stores['store_chain'] == 'LECLERC') &\
-#                          (~df_stores['store_id'].isin(ls_market_ids))]\
-#                         ['store_id'].unique().tolist()
-#
-#ls_market_2_ids = [store_id for store_id in ls_france_ids\
-#                     if store_id in ls_collected_store_ids]
-#
-#df_market_2 = df_prices[df_prices['store_id'] == ls_market_2_ids[0]]\
-#                [['section', 'family', 'product' ,'price']]
-#for store_id in ls_market_2_ids[1:]:
-#  df_market_2 = pd.merge(df_market_2,
-#                         df_prices[df_prices['store_id'] == store_id][['product', 'price']],
-#                         on = 'product',
-#                         suffixes = ('', '_{:s}'.format(store_id)),
-#                         how = 'inner')
-#df_market_2.rename(columns = {'price' : 'price_{:s}'.format(ls_market_2_ids[0])},
-#                   inplace = True)
-#
-#df_market_2_pr = df_market_2.drop(['family', 'section'], axis = 1)
-#df_market_2_pr.set_index('product', inplace = True)
-#df_market_2_pr = df_market_2_pr.T
-#
-#se_sum_2 = df_market_2_pr.sum(1)
-#print se_sum_2.to_string()
-#
-#df_market_2_su = df_market_2_pr.describe().T
-#df_market_2_su['spread'] = df_market_2_su['max'] - df_market_2_su['min']
-#df_market_2_su['gfs'] = df_market_2_su['mean'] - df_market_2_su['min']
-#df_market_2_su['cv'] = df_market_2_su['std'] / df_market_2_su['mean']
+print()
+print('Priciest store (basket share)')
+print(df_market['priciest'].value_counts(normalize = 1))
+
+# todo: compare with aggregate result
+# todo: check if cheapest always have higher share than priciest?
+# todo: check avg percentage of time cheapest is most expensive and vice versa
