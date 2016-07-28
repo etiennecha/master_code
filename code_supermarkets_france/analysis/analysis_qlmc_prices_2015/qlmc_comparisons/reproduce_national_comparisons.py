9f0efca8516e33b09bbf3c1a850be34e38367741
etna <echamayou@gmail.com>
2016-07-29 00:57:07 +0200

    update disp and compara

--- code_supermarkets_france/analysis/analysis_qlmc_prices_2015/qlmc_comparisons/reproduce_national_comparisons.py
+++ code_supermarkets_france/analysis/analysis_qlmc_prices_2015/qlmc_comparisons/reproduce_national_comparisons.py
@@ -9,9 +9,9 @@ import numpy as np
 import pandas as pd
 import timeit
 
-pd.set_option('float_format', '{:,.2f}'.format)
+pd.set_option('float_format', '{:,.1f}'.format)
 format_float_int = lambda x: '{:10,.0f}'.format(x)
-format_float_float = lambda x: '{:10,.2f}'.format(x)
+format_float_float = lambda x: '{:10,.1f}'.format(x)
 
 path_built_2015 = os.path.join(path_data,
                                'data_supermarkets',
@@ -30,18 +30,23 @@ path_built_201415_csv = os.path.join(path_built_2015,
 #ls_prod_cols = ['section', 'family', 'product']
 #store_col = 'store_id'
 
-ls_qlmc_1415_files = ['df_qlmc_201405.csv',
-                      'df_qlmc_201409.csv',
-                      'df_qlmc_201503.csv']
+ls_qlmc_dates = ['201405',
+                 '201409',
+                 '201503']
+
+date_str = ls_qlmc_dates[1]
 
 df_prices = pd.read_csv(os.path.join(path_built_201415_csv,
-                                     ls_qlmc_1415_files[2]),
+                                     'df_qlmc_{:s}.csv'.format(date_str)),
                         encoding = 'utf-8')
-#ls_prod_cols = ['ean', 'product']
-#store_col = 'store_name'
 
-ls_prod_cols = ['section', 'family', 'product']
-store_col = 'store_id'
+
+ls_prod_cols = ['ean', 'product']
+store_col = 'store_name'
+
+if date_str == '201503':
+  ls_prod_cols = ['section', 'family', 'product']
+  store_col = 'store_id'
 
 # Costly to search by store_id within df_prices
 # hence first split df_prices in chain dataframes
@@ -110,15 +115,14 @@ for chain_a, chain_b in ls_compare_chains:
   print(u'Replicate QLMC comparison: {:s} vs {:s}'.format(chain_a, chain_b))
   res = (df_duel_sub['mean_{:s}'.format(chain_b)].mean().round(2) /\
            df_duel_sub['mean_{:s}'.format(chain_a)].mean().round(2) - 1) * 100
-  print(u'{:.1f}'.format(res))
   
   # Save both nb stores of chain b and res
   nb_stores = len(df_prices[df_prices['store_chain'] ==\
                                 chain_b][store_col].drop_duplicates())
   nb_prods = len(df_duel_sub)
-  pct_a_wins = len(df_duel_sub[df_duel_sub['diff'] > 10e-4]) / float(nb_prods)
-  pct_b_wins = len(df_duel_sub[df_duel_sub['diff'] < -10e-4]) / float(nb_prods)
-  pct_draws = len(df_duel_sub[df_duel_sub['diff'].abs() <= 10e-4]) / float(nb_prods)
+  pct_a_wins = len(df_duel_sub[df_duel_sub['diff'] > 10e-4]) / float(nb_prods) * 100
+  pct_b_wins = len(df_duel_sub[df_duel_sub['diff'] < -10e-4]) / float(nb_prods) * 100
+  pct_draws = len(df_duel_sub[df_duel_sub['diff'].abs() <= 10e-4]) / float(nb_prods) * 100
   # Save both nb stores of chain b and res
   ls_res.append([chain_b,
                  nb_stores,
@@ -136,7 +140,10 @@ for chain_a, chain_b in ls_compare_chains:
   df_duel_sub = df_duel_sub[len(df_duel_sub)/5:]
   res_2 = (df_duel_sub['mean_{:s}'.format(chain_b)].mean().round(2) /\
            df_duel_sub['mean_{:s}'.format(chain_a)].mean().round(2) - 1) * 100
-  print(u'After manip against Leclerc: {:.1f}'.format(res_2))
+  
+  print()
+  print(u'Aggregate comparison vs. Leclerc: {:.1f}%'.format(res))
+  print(u'After manip against Leclerc: {:.1f}%'.format(res_2))
 
 ##print df_duel[0:10].to_string()
 
@@ -148,39 +155,65 @@ for chain_a, chain_b in ls_compare_chains:
 # do same exercise with non leclerc pairs (control for differentiation)
 # would be nice to check if products on which leclerc is beaten are underpriced vs market
 
-ls_qlmc_official = [['AUCHAN', 140, 125, '7.6%'],
-                    ['CARREFOUR', 226, 188, '7.8%'],
-                    ['CARREFOUR MARKET', 930, 421, '13.5%'],
-                    ['CASINO', 388, 151, '16.7%'],
-                    ['CORA', 58, 58, '10.2%'],
-                    ['GEANT CASINO', 108, 108, '1.8%'],
-                    ['LECLERC', 581, 581, ''],
-                    ['INTERMARCHE', 1815, 1022, '7%'],
-                    ['SIMPLY MARKET', 303, 50, '12.9%'],
-                    ['SYSTEME U', 1044, 632, '6.7%']]
+dict_qlmc_official = {}
 
-df_recap = pd.DataFrame(ls_qlmc_official,
-                        columns = ['Chain',
-                                   'Nb stores',
-                                   'Nb stores in QLMC',
-                                   'QLMC: vs. LEC'])
-df_recap.set_index('Chain', inplace = True)
+dict_qlmc_official['201503'] = [['AUCHAN'          ,  140,  125,  '7.6%'],
+                                ['CARREFOUR'       ,  226,  188,  '7.8%'],
+                                ['CARREFOUR MARKET',  930,  421, '13.5%'],
+                                ['CASINO'          ,  388,  151, '16.7%'],
+                                ['CORA'            ,   58,   58, '10.2%'],
+                                ['GEANT CASINO'    ,  108,  108,  '1.8%'],
+                                ['LECLERC'         ,  581,  581,      ''],
+                                ['INTERMARCHE'     , 1815, 1022,  '7.0%'],
+                                ['SIMPLY MARKET'   ,  303,   50, '12.9%'],
+                                ['SYSTEME U'       , 1044,  632,  '6.7%']]
+
+dict_qlmc_official['201409'] = [['AUCHAN'          ,  140,    108,  '6.0%'],
+                                ['CARREFOUR'       ,  226,    180,  '2.6%'],
+                                ['CARREFOUR MARKET',  930,    210, '12.0%'],
+                                ['CASINO'          ,  388, np.nan,      ''],
+                                ['CORA'            ,   58,     54,  '9.3%'],
+                                ['GEANT CASINO'    ,  108,     95,  '2.4%'],
+                                ['LECLERC'         ,  581,    576,      ''],
+                                ['INTERMARCHE'     , 1815,    470,  '6.2%'],
+                                ['SIMPLY MARKET'   ,  303, np.nan,      ''],
+                                ['SYSTEME U'       , 1044,    336,  '5.3%']]
+
+dict_qlmc_official['201402'] = [['AUCHAN'          ,  140,    107,  '5.5%'],
+                                ['CARREFOUR'       ,  226,    182,  '3.0%'],
+                                ['CARREFOUR MARKET',  930,    208,  '8.8%'],
+                                ['CASINO'          ,  388, np.nan,      ''],
+                                ['CORA'            ,   58,     54, '10.1%'],
+                                ['GEANT CASINO'    ,  108,     94,  '2.9%'],
+                                ['LECLERC'         ,  581,    572,      ''],
+                                ['INTERMARCHE'     , 1815,    458,  '3.8%'],
+                                ['SIMPLY MARKET'   ,  303, np.nan,      ''],
+                                ['SYSTEME U'       , 1044,    332,  '4.6%']]
+
+dict_qlmc_official['201405'] = dict_qlmc_official['201402'] # a bit of a delay...
+
+df_recap = pd.DataFrame(dict_qlmc_official.get(date_str, None),
+                        columns = [u'Chain',
+                                   u'Nb stores (LSA)',
+                                   u'Nb stores (QLMC)',
+                                   u'COMP vs. LEC'])
+df_recap.set_index(u'Chain', inplace = True)
 
 # Merge with my results
 df_res = pd.DataFrame(ls_res,
-                      columns = ['Chain',
-                                 'Nb stores (my data)',
-                                 'vs. LEC (my data)',
-                                 'Nb prods',
-                                 'Pct Leclerc wins',
-                                 'Pct Competitor wins',
-                                 'Pct draws'])
-df_res.set_index('Chain', inplace = True)
-df_res.rename(index = {'SUPER U' : 'SYSTEME U'}, inplace = True)
-df_res['vs. LEC (my data)'].apply(lambda x: u'{:.1f}%'.format(x))
-df_res.loc[u'LECLERC', u'Nb stores (my data)'] =\
+                      columns = [u'Chain',
+                                 u'Nb stores (data)',
+                                 u'COMP vs. LEC (data)',
+                                 u'Nb prods',
+                                 u'LEC wins (%)',
+                                 u'COMP wins (%)',
+                                 u'DRAWS (%)'])
+df_res.set_index(u'Chain', inplace = True)
+df_res.rename(index = {u'SUPER U' : u'SYSTEME U'}, inplace = True)
+df_res[u'COMP vs. LEC (data)'].apply(lambda x: u'{:.1f}%'.format(x))
+df_res.loc[u'LECLERC', u'Nb stores (data)'] =\
               len(df_prices[df_prices['store_chain'] ==\
-                                'LECLERC'][store_col].drop_duplicates())
+                              u'LECLERC'][store_col].drop_duplicates())
 
 df_recap = pd.merge(df_recap,
                     df_res,
@@ -188,6 +221,11 @@ df_recap = pd.merge(df_recap,
                     right_index = True,
                     how = 'outer')
 
+pd.set_option('float_format', '{:,.0f}'.format)
+
 print()
-print(u'Summary of March 2015 national comparisons:')
-print(df_recap.to_string())
+print(u'Summary of {:s} national comparisons:'.format(date_str))
+print(df_recap.to_string(formatters = {u'COMP vs. LEC (data)' : format_float_float,
+                                       u'LEC wins (%)' : format_float_float,
+                                       u'COMP wins (%)' : format_float_float,
+                                       u'DRAWS (%)': format_float_float}))
