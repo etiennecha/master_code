b886ca8cc203c6977013c2ffafbdd532c31b54a4
etiennecha <echamayou@gmail.com>
2016-10-26 17:53:06 +0200

    add market maps

---
+++ code_gasoline_france/analysis/gen_geography/map_markets.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import add_to_path
+from add_to_path import path_data
+from generic_master_price import *
+from generic_master_info import *
+from functions_maps import *
+from mpl_toolkits.basemap import Basemap
+from shapely.geometry import Point, Polygon, MultiPoint, MultiPolygon, shape
+from shapely.prepared import prep
+from descartes import PolygonPatch
+from matplotlib.text import TextPath
+
+path_dir_built = os.path.join(path_data,
+                              'data_gasoline',
+                              'data_built',
+                              'data_scraped_2011_2014')
+path_dir_built_json = os.path.join(path_dir_built, 'data_json')
+path_dir_built_csv = os.path.join(path_dir_built, u'data_csv')
+path_dir_built_graphs = os.path.join(path_dir_built, u'data_graphs')
+
+path_dir_insee_extracts = os.path.join(path_data,
+                                       'data_insee',
+                                       'data_extracts')
+
+
+# ###############
+# LOAD DATA
+# ###############
+
+# LOAD DF INFO AND PRICES
+
+df_info = pd.read_csv(os.path.join(path_dir_built_csv,
+                                   'df_station_info_final.csv'),
+                      encoding = 'utf-8',
+                      dtype = {'id_station' : str,
+                               'adr_zip' : str,
+                               'adr_dpt' : str,
+                               'ci_1' : str,
+                               'ci_ardt_1' :str,
+                               'ci_2' : str,
+                               'ci_ardt_2' : str,
+                               'dpt' : str},
+                      parse_dates = ['start', 'end', 'day_0', 'day_1', 'day_2'])
+df_info.set_index('id_station', inplace = True)
+# Get rid of highway (gps too unreliable so far + require diff treatment)
+df_info = df_info[df_info['highway'] != 1]
+
+# LOAD PRICES (only for stats des...)
+df_prices_ht = pd.read_csv(os.path.join(path_dir_built_csv,
+                                        'df_prices_ht_final.csv'),
+                           parse_dates = ['date'])
+df_prices_ht.set_index('date', inplace = True)
+
+df_prices_ttc = pd.read_csv(os.path.join(path_dir_built_csv,
+                                         'df_prices_ttc_final.csv'),
+                            parse_dates = ['date'])
+df_prices_ttc.set_index('date', inplace = True)
+
+# LOAD JSON CLOSE
+
+dict_ls_close = dec_json(os.path.join(path_dir_built_json,
+                                      'dict_ls_close.json'))
+ls_close_pairs = dec_json(os.path.join(path_dir_built_json,
+                                      'ls_close_pairs.json'))
+
+# COMPETITORS
+ls_comp_pairs = dec_json(os.path.join(path_dir_built_json,
+                                      'ls_comp_pairs.json'))
+
+dict_ls_comp = dec_json(os.path.join(path_dir_built_json,
+                                     'dict_ls_comp.json'))
+
+# NB BASED ON INSEE AREAS
+df_insee_areas = pd.read_csv(os.path.join(path_dir_insee_extracts,
+                                          'df_insee_areas.csv'),
+                             dtype = {'CODGEO' : str,
+                                      'AU2010': str,
+                                      'UU2010': str,
+                                      'BV' : str},
+                             encoding = 'utf-8')
+
+df_info = df_info.reset_index().merge(df_insee_areas[['CODGEO', 'AU2010', 'UU2010', 'BV']],
+                                      left_on = 'ci_1', right_on = 'CODGEO',
+                                      how = 'left').set_index('id_station')
+
+ls_areas = ['ci_1', 'AU2010', 'UU2010', 'BV']
+df_area_nb = df_info[ls_areas].copy()
+df_area_nb.reset_index(inplace = True) # index used to add data by area
+for area in ls_areas:
+  se_area = df_info[area].value_counts()
+  df_area_nb.set_index(area, inplace = True)
+  df_area_nb['nb_%s' %area] = se_area
+df_area_nb.set_index('id_station', inplace = True)
+
+# NB SAME GROUP IN INSEE AREAS
+ls_areas = ['ci_1', 'AU2010', 'UU2010', 'BV']
+df_area_same = df_info[['group'] + ls_areas].copy()
+ls_groups = df_info['group'][~(df_info['group'].isnull())].unique()
+for group_name in ls_groups:
+  df_group = df_info[df_info['group'] == group_name]
+  for area in ls_areas:
+    se_group_area = df_group[area].value_counts()
+    df_area_same.reset_index(inplace = True)
+    df_area_same.set_index(area, inplace = True)
+    df_area_same.loc[df_area_same['group'] == group_name,
+                     'nb_s_%s' %area] = se_group_area
+df_area_same.set_index('id_station', inplace = True)
+df_area_same = df_area_same[['nb_s_%s' %area for area in ls_areas]]
+
+# MARKETS ON THEIR OWN
+ls_dict_markets = dec_json(os.path.join(path_dir_built_json,
+                                        'ls_dict_stable_markets.json'))
+
+ls_robust_markets = dec_json(os.path.join(path_dir_built_json,
+                                          'ls_robust_stable_markets.json'))
+
+# ####
+# MAPS
+# ####
+
+dict_robust_markets = {}
+for x in ls_robust_markets:
+  dict_robust_markets.setdefault(len(x), []).append(x)
+
+for ls_market_ids in dict_robust_markets[6]:
+  
+  ls_close_ids = [x[0] for market_id in ls_market_ids for x in dict_ls_close[market_id]]
+  ls_outside_ids = list(set(ls_close_ids).difference(set(ls_market_ids)))
+  
+  ls_market_coordinates = [df_info.ix[x][['lat', 'lng']].tolist() for x in ls_market_ids]
+  lat_ref = (np.max([x[0] for x in ls_market_coordinates]) +\
+               np.min([x[0] for x in ls_market_coordinates])) / 2
+  lng_ref = (np.max([x[1] for x in ls_market_coordinates]) +\
+               np.min([x[1] for x in ls_market_coordinates])) / 2
+  
+  ls_outside_coordinates = [df_info.ix[x][['lat', 'lng']].tolist() for x in ls_outside_ids]
+  
+  zoom = 12
+  # todo: see how to get dist of X km?
+  delta_lat = 0.2 # height
+  delta_lng = 0.2 # width
+  
+  a, bbox = getImageCluster(lat_ref - delta_lat, # need 1/2 delta_lat
+                            lng_ref - delta_lng, # need 1/2 delta_lng
+                            delta_lat * 2,
+                            delta_lng * 2,
+                            zoom)
+  
+  fig = plt.figure(figsize=(10, 10))
+  ax = plt.subplot(111)
+  m = Basemap(
+      llcrnrlon=bbox[0], llcrnrlat=bbox[1],
+      urcrnrlon=bbox[2], urcrnrlat=bbox[3],
+      projection='merc', ax=ax)
+  
+  # display image composed of OSM times
+  m.imshow(a, interpolation='lanczos', origin='upper')
+  
+  # convert coord (caution: inversion)
+  for ls_gps_points, marker_c in [[ls_market_coordinates, 'b'],
+                                  [ls_outside_coordinates, 'r']]:
+    ls_points = [m(x[1], x[0]) for x in ls_gps_points]
+    ax.scatter([point[0] for point in ls_points],
+               [point[1] for point in ls_points],
+               alpha = 0.8, s = 50, color = marker_c, zorder = 9)
+  
+  # add circles
+  def radius_for_tissot(dist_km):
+      return np.rad2deg(dist_km/6367.)
+  
+  for ls_gps_points, marker_c, radius in [[ls_market_coordinates, 'b', 3],
+                                          [ls_outside_coordinates, 'r', 5]]:
+    for lat_x, lng_x in ls_gps_points:
+      m.tissot(lng_x,
+               lat_x,
+               radius_for_tissot(radius),
+               256, facecolor=marker_c, alpha=0.1, zorder = 8)
+  
+  # need to cut again (otherwise get whole loaded tiles displayed)
+  xmin, ymin = m(lng_ref - delta_lng, lat_ref - delta_lat)
+  xmax, ymax = m(lng_ref + delta_lng, lat_ref + delta_lat)
+  
+  ax.set_xlim((xmin, xmax))
+  ax.set_ylim((ymin, ymax))
+  
+  plt.tight_layout()
+  #plt.show()
+  
+  plt.savefig(os.path.join(path_dir_built_graphs,
+                           'Market_6_{:s}.png'.format(ls_market_ids[0])),
+              dpi=300,
+              alpha=True,
+              bbox_inches = 'tight')
---
+++ code_gasoline_france/functions/functions_maps.py
@@ -0,0 +1,174 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from mpl_toolkits.basemap import Basemap
+from shapely.geometry import Point, Polygon, MultiPoint, MultiPolygon, shape
+from descartes import PolygonPatch
+from matplotlib.colors import Normalize
+from matplotlib.collections import PatchCollection
+import matplotlib.font_manager as fm
+from pysal.esda.mapclassify import Natural_Breaks as nb
+from matplotlib import colors
+import math
+import urllib2
+import StringIO
+from PIL import Image
+
+class GeoFrance:
+  def __init__(self, path_com = None, path_dpt = None, corse = False):
+    """
+    @param file_name: path to a csv file containing citys names, zips, dpts, and insee codes
+    @type file_name: C{str}
+    """
+    x1 = -5.
+    x2 = 9.
+    y1 = 42.
+    y2 = 51.5
+    
+    # Lambert conformal for France (as suggested by IGN... check WGS84 though?)
+    self.m_fra = Basemap(resolution='i',
+                         projection='lcc',
+                         ellps = 'WGS84',
+                         lat_1 = 44.,
+                         lat_2 = 49.,
+                         lat_0 = 46.5,
+                         lon_0 = 3,
+                         llcrnrlat=y1,
+                         urcrnrlat=y2,
+                         llcrnrlon=x1,
+                         urcrnrlon=x2)
+    
+    # Departments: one departement can be several lines (??) (several polygons)
+    if path_dpt:
+      self.m_fra.readshapefile(path_dpt, 'dpt', color = 'none', zorder=2)
+    self.df_dpt = pd.DataFrame({'poly'          : [Polygon(xy) for xy in self.m_fra.dpt],
+                                'departement'   : [d['NOM_DEPT'] for d in self.m_fra.dpt_info],
+                                'c_departement' : [d['CODE_DEPT'] for d in self.m_fra.dpt_info],
+                                'region'        : [d['NOM_REGION'] for d in self.m_fra.dpt_info],
+                                'c_region'      : [d['CODE_REG'] for d in self.m_fra.dpt_info]})
+    if not corse:
+      self.df_dpt = self.df_dpt[self.df_dpt['region'] != 'CORSE']
+    
+    # Communes: one commune can be several lines (several polygons)
+    self.m_fra.readshapefile(path_com, 'com', color = 'none', zorder=2)
+    self.df_com = pd.DataFrame({'poly'         : [Polygon(xy) for xy in self.m_fra.com],
+                                'c_insee'      : [d['INSEE_COM'] for d in self.m_fra.com_info],
+                                'commune'      : [d['NOM_COMM'] for d in self.m_fra.com_info],
+                                'departement'  : [d['NOM_DEPT'] for d in self.m_fra.com_info],
+                                'region'       : [d['NOM_REGION'] for d in self.m_fra.com_info],
+                                'com_pop'      : [d['POPULATION'] for d in self.m_fra.com_info],
+                                'com_surf'     : [d['SUPERFICIE'] for d in self.m_fra.com_info],
+                                'x_ct'         : [d['X_CENTROID'] for d in self.m_fra.com_info],
+                                'y_ct'         : [d['Y_CENTROID'] for d in self.m_fra.com_info],
+                                'x_cl'         : [d['X_CHF_LIEU'] for d in self.m_fra.com_info],
+                                'y_cl'         : [d['Y_CHF_LIEU'] for d in self.m_fra.com_info]})
+    if not corse:
+      self.df_com = self.df_com[self.df_com['region'] != 'CORSE']
+
+  def convert_gps_to_ign(self, x, y):
+    x_l_93_ign = (self.m_fra(x, y)[0] + 700000 - self.m_fra(3, 46.5)[0])/100.0
+    y_l_93_ign = (self.m_fra(x, y)[1] + 6600000 - self.m_fra(3, 46.5)[1])/100.0
+    return x_l_93_ign, y_l_93_ign
+  
+  def convert_ign_to_gps(self, x_l_93_ign, y_l_93_ign):
+    x = x_l_93_ign * 100 - 700000 + self.m_fra(3, 46.5)[0] 
+    y = y_l_93_ign * 100 - 6600000 + self.m_fra(3, 46.5)[1]
+    x, y = self.m_fra(x, y, inverse = True)
+    return x, y
+
+def colorbar_index(ncolors, cmap, labels=None, **kwargs):
+  """
+  This is a convenience function to stop you making off-by-one errors
+  Takes a standard colour ramp, and discretizes it,
+  then draws a colour bar with correctly aligned labels
+  """
+  cmap = cmap_discretize(cmap, ncolors)
+  mappable = cm.ScalarMappable(cmap=cmap)
+  mappable.set_array([])
+  mappable.set_clim(-0.5, ncolors+0.5)
+  colorbar = plt.colorbar(mappable, **kwargs)
+  colorbar.set_ticks(np.linspace(0, ncolors, ncolors))
+  colorbar.set_ticklabels(range(ncolors))
+  if labels:
+    colorbar.set_ticklabels(labels)
+  return colorbar
+
+def cmap_discretize(cmap, N):
+  """
+  Return a discrete colormap from the continuous colormap cmap.
+      
+    cmap: colormap instance, eg. cm.jet. 
+    N: number of colors.
+  
+  Example
+    x = resize(arange(100), (5,100))
+    djet = cmap_discretize(cm.jet, 5)
+    imshow(x, cmap=djet)
+  
+  """
+  if type(cmap) == str:
+    cmap = get_cmap(cmap) #get_cmap? probably cm.get_cmap()
+  colors_i = np.concatenate((np.linspace(0, 1., N), (0., 0., 0., 0.)))
+  colors_rgba = cmap(colors_i)
+  indices = np.linspace(0, 1., N + 1)
+  cdict = {}
+  for ki, key in enumerate(('red', 'green', 'blue')):
+    cdict[key] = [(indices[i], colors_rgba[i - 1, ki], colors_rgba[i, ki]) for i in xrange(N + 1)]
+  return matplotlib.colors.LinearSegmentedColormap(cmap.name + "_%d" % N, cdict, 1024)
+
+def deg2num(lat_deg, lon_deg, zoom):
+  """
+  Tiles to request (OSM) based on gps coordinates and zoom
+  """
+  lat_rad = math.radians(lat_deg)
+  n = 2.0 ** zoom
+  xtile = int((lon_deg + 180.0) / 360.0 * n)
+  ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
+  return (xtile, ytile)
+
+def num2deg(xtile, ytile, zoom):
+  """
+  This returns the NW-corner of the square. 
+  Use the function with xtile+1 and/or ytile+1 to get the other corners. 
+  With xtile+0.5 & ytile+0.5 it will return the center of the tile.
+  """
+  n = 2.0 ** zoom
+  lon_deg = xtile / n * 360.0 - 180.0
+  lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+  lat_deg = math.degrees(lat_rad)
+  return (lat_deg, lon_deg)
+
+def getImageCluster(lat_deg, lon_deg, delta_lat,  delta_long, zoom):
+    #base_url = r"http://otile1.mqcdn.com/tiles/1.0.0/map/"
+    base_url= r"http://a.tile.openstreetmap.org/"
+    smurl = base_url + r"{0}/{1}/{2}.png"
+    xmin, ymax = deg2num(lat_deg, lon_deg, zoom)
+    xmax, ymin = deg2num(lat_deg + delta_lat, lon_deg + delta_long, zoom)
+
+    bbox_ul = num2deg(xmin, ymin, zoom)
+    bbox_ll = num2deg(xmin, ymax + 1, zoom)
+    #print bbox_ul, bbox_ll
+
+    bbox_ur = num2deg(xmax + 1, ymin, zoom)
+    bbox_lr = num2deg(xmax + 1, ymax +1, zoom)
+    #print bbox_ur, bbox_lr
+
+    Cluster = Image.new('RGB',((xmax-xmin+1)*256-1,(ymax-ymin+1)*256-1) )
+    for xtile in range(xmin, xmax+1):
+        for ytile in range(ymin,  ymax+1):
+            try:
+                imgurl=smurl.format(zoom, xtile, ytile)
+                print("Opening: " + imgurl)
+                imgstr = urllib2.urlopen(imgurl).read()
+                tile = Image.open(StringIO.StringIO(imgstr))
+                Cluster.paste(tile, box=((xtile-xmin)*255 ,  (ytile-ymin)*255))
+            except: 
+                print("Couldn't download image")
+                tile = None
+
+    return Cluster, [bbox_ll[1], bbox_ll[0], bbox_ur[1], bbox_ur[0]]
