891e27d2c8ff4d90a78f066605de2f570ce18614
etna <echamayou@gmail.com>
2017-06-17 10:15:08 +0200

    drop old dispersion and total access dirs

--- code_gasoline_france/bu_execution_total_access/creation_db/creation_db_gouv/params.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-# Define location of built data (must contain folders data_json, data_csv)
-data_paper_folder = 'data_paper_total_access'
-
-# Define start and end dates of period covered (%Y, %m, %d)
-start_date = (2011, 9, 4)
-end_date = (2014, 12, 4)
